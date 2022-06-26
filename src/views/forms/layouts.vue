8a590541fdf9dff4c3a36662a35c8772a1c349b4
Simon9991 <simon.bernabeu@epitech.eu>
2022-06-26 15:07:47 +0200

    Updated a lot (check details)
    Added Dropzone component
    Updated feedback form with files upload
    Added Feedback from to Bug report page

--- src/views/forms/layouts.vue
+++ src/views/forms/layouts.vue
@@ -1,15 +1,44 @@
 <script>
+import { ref, watch } from "vue";
+import useVuelidate from "@vuelidate/core";
 import "flatpickr/dist/flatpickr.css";
 import "@vueform/multiselect/themes/default.css";
+
+import DropZone from "@/components/widgets/dropZone";
 
 import Layout from "../../layouts/main.vue";
 import PageHeader from "@/components/page-header";
 import appConfig from "../../../app.config";
 
 export default {
+  methods: {
+    deleteRecord(ele) {
+      ele.target.parentElement.parentElement.remove();
+    },
+  },
   page: {
     title: "Feedback",
     meta: [{ name: "description", content: appConfig.description }],
+  },
+  setup() {
+    let files = ref([]);
+    let dropzoneFile = ref("");
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0];
+      files.value.push(dropzoneFile.value);
+    };
+    const selectedFile = () => {
+      dropzoneFile.value = document.querySelector(".dropzoneFile").files[0];
+      files.value.push(dropzoneFile.value);
+    };
+    watch(
+      () => [...files.value],
+      (currentValue) => {
+        return currentValue;
+      }
+    );
+
+    return { dropzoneFile, files, drop, selectedFile, v$: useVuelidate() };
   },
   data() {
     return {
@@ -43,7 +72,8 @@ export default {
   },
   components: {
     Layout,
-    PageHeader
+    PageHeader,
+    DropZone,
   },
   mounted() {
     const checkbox = document.getElementsByClassName("code-switcher");
@@ -70,9 +100,8 @@ export default {
 <template>
   <Layout>
     <PageHeader :title="title" :items="items" />
+
     <div class="row">
-
-
       <div class="col">
         <div class="card">
           <div class="card-header align-items-center d-flex">
@@ -91,7 +120,42 @@ export default {
                 <div class="mb-6">
                   <label for="VertimeassageInput" class="form-label">Message</label>
                   <textarea class="form-control" id="VertimeassageInput" rows="3" placeholder="Enter your feedback here"
-                    required="true"></textarea>
+                    required></textarea>
+                  <div class="invalid-feedback">
+                    Please enter a message in the textarea.
+                  </div>
+                </div>
+
+                <!-- File Upload Dropzone -->
+                <div class="card-body">
+                  <p class="text-muted">
+                    You can upload files to help us understand your Feeback better.
+                  </p>
+
+                  <DropZone @drop.prevent="drop" @change="selectedFile" />
+                  <ul class="list-unstyled mb-0" id="dropzone-preview">
+                    <div class="border rounded" v-for="(file, index) of files" :key="index">
+                      <div class="d-flex p-2">
+                        <div class="flex-grow-1">
+                          <div class="pt-1">
+                            <h5 class="fs-14 mb-1" data-dz-name="">
+                              {{ file.name }}
+                            </h5>
+                            <p class="fs-13 text-muted mb-0" data-dz-size="">
+                              <strong>{{ file.size / 1024 }}</strong> KB
+                            </p>
+                            <strong class="error text-danger" data-dz-errormessage=""></strong>
+                          </div>
+                        </div>
+                        <div class="flex-shrink-0 ms-3">
+                          <button data-dz-remove="" class="btn btn-sm btn-danger" @click="deleteRecord">
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </ul>
+                  <!-- end dropzon-preview -->
                 </div>
 
                 <!-- Feedback category multiselect -->
@@ -116,8 +180,8 @@ export default {
                 <div class="col-md-4">
 
                   <div class="col-auto">
-                    <label for="chooseCategorSelect" class="form-label">Category</label>
-                    <select class="form-select" id="chooseCategorSelect" required>
+                    <label for="chooseCategorySelect" class="form-label">Category</label>
+                    <select class="form-select" id="chooseCategorySelect" required>
                       <option selected value="login">Login Page</option>
                       <option value="dashboard">Home Page</option>
                       <option value="calendar">Calendar Page</option>
@@ -128,6 +192,7 @@ export default {
                       <option value="locked">Locked Login Page</option>
                       <option value="tou">Terms of Use</option>
                     </select>
+                    <div class="invalid-feedback">Choose a category</div>
                   </div>
                 </div>
 
@@ -168,11 +233,12 @@ export default {
                 <!-- Agree on ToU check box -->
                 <div class="col-12">
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck" required="true" />
+                    <input class="form-check-input" type="checkbox" id="gridCheck" required />
                     <label class="form-check-label" for="gridCheck">
                       I have read and accepted the Terms of Use
                     </label>
                   </div>
+                  <div class="invalid-feedback">You have to check this box</div>
                 </div>
 
                 <!-- Submit button -->
