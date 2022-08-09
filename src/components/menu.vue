3ecd8e9982f505d7fadda37e4fde45ef056ceb07
Simon9991 <simon.bernabeu@epitech.eu>
2022-08-09 18:47:52 +0200

    Changed icons: Dashboard, Medical Information Page

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -151,13 +151,13 @@ export default {
         </li>
         <li class="nav-item">
           <router-link class="nav-link menu-link" to="/">
-            <em class="ri-dashboard-2-line"></em>
+            <em class="ri-home-8-line"></em>
             <span style="padding: 7px" data-key="t-home">{{ $t("t-home") }}</span>
           </router-link>
         </li>
         <li class="nav-item">
           <router-link class="nav-link menu-link" to="/medical-information">
-            <em class="ri-layout-grid-line"></em>
+            <em class="ri-information-line"></em>
             <span style="padding: 7px" data-key="t-medicalinformation">{{ $t("t-medicalinformation") }}</span>
           </router-link>
         </li>
