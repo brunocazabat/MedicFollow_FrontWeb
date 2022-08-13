52b4ca39f6b9037f85a9fdb0a8f7fb5fc09a3436
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-14 00:51:31 +0200

    Test avec un cover signup

--- src/views/auth/signup/basic.vue
+++ src/views/auth/signup/basic.vue
@@ -1,313 +1,121 @@
 <script>
-export default {
-  methods: {},
-  mounted() {
-    var password = document.getElementById('password-input')
-    password.onchange = this.validatePassword
-    document
-      .querySelectorAll('form .auth-pass-inputgroup')
-      .forEach(function (item) {
-        item.querySelectorAll('.password-addon').forEach(function (subitem) {
-          subitem.addEventListener('click', function () {
-            var passwordInput = item.querySelector('.password-input')
-            if (passwordInput.type === 'password') {
-              passwordInput.type = 'text'
-            } else {
-              passwordInput.type = 'password'
-            }
-          })
-        })
-      })
-
-    var myInput = document.getElementById('password-input')
-    var letter = document.getElementById('pass-lower')
-    var capital = document.getElementById('pass-upper')
-    var number = document.getElementById('pass-number')
-    var length = document.getElementById('pass-length')
-
-    // When the user clicks on the password field, show the message box
-    myInput.onfocus = function () {
-      document.getElementById('password-contain').style.display = 'block'
-    }
-
-    // When the user clicks outside of the password field, hide the password-contain box
-    myInput.onblur = function () {
-      document.getElementById('password-contain').style.display = 'none'
-    }
-
-    // When the user starts to type something inside the password field
-    myInput.onkeyup = function () {
-      // Validate lowercase letters
-      var lowerCaseLetters = /[a-z]/g
-      if (myInput.value.match(lowerCaseLetters)) {
-        letter.classList.remove('invalid')
-        letter.classList.add('valid')
-      } else {
-        letter.classList.remove('valid')
-        letter.classList.add('invalid')
-      }
-
-      // Validate capital letters
-      var upperCaseLetters = /[A-Z]/g
-      if (myInput.value.match(upperCaseLetters)) {
-        capital.classList.remove('invalid')
-        capital.classList.add('valid')
-      } else {
-        capital.classList.remove('valid')
-        capital.classList.add('invalid')
-      }
-
-      // Validate numbers
-      var numbers = /[0-9]/g
-      if (myInput.value.match(numbers)) {
-        number.classList.remove('invalid')
-        number.classList.add('valid')
-      } else {
-        number.classList.remove('valid')
-        number.classList.add('invalid')
-      }
-
-      // Validate length
-      if (myInput.value.length >= 8) {
-        length.classList.remove('invalid')
-        length.classList.add('valid')
-      } else {
-        length.classList.remove('valid')
-        length.classList.add('invalid')
-      }
-    }
-  }
-}
+export default {};
 </script>
 
 <template>
-  <div class="auth-page-wrapper pt-5">
-    <!-- auth page bg -->
-    <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-      <div class="bg-overlay"></div>
-
-      <div class="shape">
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 1440 120">
-          <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-        </svg>
-      </div>
-      <Particles id="tsparticles" :options="{
-        particles: {
-          number: {
-            value: 90,
-            density: {
-              enable: true,
-              value_area: 800
-            }
-          },
-          color: {
-            value: '#ffffff'
-          },
-          shape: {
-            type: 'circle',
-            stroke: {
-              width: 0,
-              color: '#000000'
-            },
-            polygon: {
-              nb_sides: 5
-            },
-            image: {
-              src: 'img/github.svg',
-              width: 100,
-              height: 100
-            }
-          },
-          opacity: {
-            value: 0.8,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0,
-              sync: false
-            }
-          },
-          size: {
-            value: 4,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 4,
-              size_min: 0.2,
-              sync: false
-            }
-          },
-          line_linked: {
-            enable: false,
-            distance: 150,
-            color: '#ffffff',
-            opacity: 0.4,
-            width: 1
-          },
-          move: {
-            enable: true,
-            speed: 2,
-            direction: 'none',
-            random: false,
-            straight: false,
-            out_mode: 'out',
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200
-            }
-          }
-        },
-        interactivity: {
-          detect_on: 'canvas',
-          events: {
-            onhover: {
-              enable: true,
-              mode: 'bubble'
-            },
-            onclick: {
-              enable: true,
-              mode: 'repulse'
-            },
-            resize: true
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 1
-              }
-            },
-            bubble: {
-              distance: 400,
-              size: 4,
-              duration: 2,
-              opacity: 0.8,
-              speed: 3
-            },
-            repulse: {
-              distance: 200
-            },
-            push: {
-              particles_nb: 4
-            },
-            remove: {
-              particles_nb: 2
-            }
-          }
-        },
-        retina_detect: true,
-        config_demo: {
-          hide_card: false,
-          background_color: '#b61924',
-          background_image: '',
-          background_position: '50% 50%',
-          background_repeat: 'no-repeat',
-          background_size: 'cover'
-        }
-      }" />
-    </div>
-
-    <!-- auth page content -->
-    <div class="auth-page-content">
+  <!-- auth-page wrapper -->
+  <div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
+    <div class="bg-overlay"></div>
+    <!-- auth-page content -->
+    <div class="auth-page-content overflow-hidden pt-lg-5">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
-            <div class="text-center mt-sm-5 mb-4 text-white-50">
-              <div>
-                <router-link to="/" class="d-inline-block auth-logo">
-                  <img src="@/assets/images/logo/logo-long.png" alt="" height="20" />
-                </router-link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- end row -->
-
-        <div class="row justify-content-center">
-          <div class="col-md-8 col-lg-6 col-xl-5">
-            <div class="card mt-4">
-              <div class="card-body p-4">
-                <div class="text-center mt-2">
-                  <h5 class="text-primary">Create New Account</h5>
-                  <p class="text-muted">Get your free velzon account now</p>
-                </div>
-                <div class="p-2 mt-4">
-                  <form class="needs-validation" novalidate>
-                    <div class="mb-3">
-                      <label for="useremail" class="form-label">Email <span class="text-danger">*</span></label>
-                      <input type="email" class="form-control" id="useremail" placeholder="Enter email address"
-                        required />
-                      <div class="invalid-feedback">Please enter email</div>
-                    </div>
-                    <div class="mb-3">
-                      <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
-                      <input type="text" class="form-control" id="username" placeholder="Enter username" required />
-                      <div class="invalid-feedback">Please enter username</div>
-                    </div>
-
-                    <div class="mb-3">
-                      <label class="form-label" for="password-input">Password</label>
-                      <div class="position-relative auth-pass-inputgroup">
-                        <input type="password" class="form-control pe-5 password-input" onpaste="return false"
-                          placeholder="Enter password" id="password-input" aria-describedby="passwordInput"
-                          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required />
-                        <button
-                          class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon"
-                          type="button" id="password-addon">
-                          <em class="ri-eye-fill align-middle"></em>
-                        </button>
-                        <div class="invalid-feedback">
-                          Please enter password
+            <div class="card overflow-hidden">
+              <div class="row g-0">
+                <div class="col-lg-6">
+                  <div class="p-lg-5 p-4 auth-one-bg h-100">
+                    <div class="bg-overlay"></div>
+                    <div class="position-relative h-100 d-flex flex-column">
+                      <div class="mt-auto">
+                        <div class="carousel slide">
+                          <div class="carousel-inner text-center text-white pb-5">
+                            <div class="carousel-item active">
+                              <p class="fs-15 fst-italic">
+                                " Et zalut ça va "
+                              </p>
+                            </div>
+                          </div>
                         </div>
+                        <!-- end carousel -->
                       </div>
                     </div>
-
-                    <div class="mb-4">
-                      <p class="mb-0 fs-12 text-muted fst-italic">
-                        By registering you agree to the MedicFollow
-                        <a href="#" class="text-primary text-decoration-underline fst-normal fw-medium">Terms of Use</a>
-                      </p>
-                    </div>
-                    <div id="password-contain" class="p-3 bg-light mb-2 rounded">
-                      <h5 class="fs-13">Password must contain:</h5>
-                      <p id="pass-length" class="invalid fs-12 mb-2">
-                        Minimum <strong>8 characters</strong>
-                      </p>
-                      <p id="pass-lower" class="invalid fs-12 mb-2">
-                        At <strong>lowercase</strong> letter (a-z)
-                      </p>
-                      <p id="pass-upper" class="invalid fs-12 mb-2">
-                        At least <strong>uppercase</strong> letter (A-Z)
-                      </p>
-                      <p id="pass-number" class="invalid fs-12 mb-0">
-                        A least <strong>number</strong> (0-9)
-                      </p>
-                    </div>
-                    <div class="mt-4">
-                      <button class="btn btn-success w-100" type="button">
-                        Sign Up
-                      </button>
-                    </div>
-
-                  </form>
+                  </div>
                 </div>
+                <!-- end col -->
+
+                <div class="col-lg-6">
+                  <div class="p-lg-5 p-4">
+                    <div>
+                      <h5 class="text-primary">Welcome Back !</h5>
+                      <p class="text-muted">
+                        Sign in to continue to MedicFollow.
+                      </p>
+                    </div>
+
+                    <div class="mt-4">
+                      <form>
+                        <div class="mb-3">
+                          <label for="username" class="form-label">Username</label>
+                          <input type="text" class="form-control" id="username" placeholder="Enter username" />
+                        </div>
+
+                        <div class="mb-3">
+                          <div class="float-end">
+                            <router-link to="/auth/reset-pwd-cover" class="text-muted">Forgot password?</router-link>
+                          </div>
+                          <label class="form-label" for="password-input">Password</label>
+                          <div class="position-relative auth-pass-inputgroup mb-3">
+                            <input type="password" class="form-control pe-5" placeholder="Enter password"
+                              id="password-input" />
+                            <button
+                              class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted shadow-none"
+                              type="button" id="password-addon">
+                              <em class="ri-eye-fill align-middle"></em>
+                            </button>
+                          </div>
+                        </div>
+
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="auth-remember-check" />
+                          <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                        </div>
+
+                        <div class="mt-4">
+                          <button class="btn btn-success w-100" type="submit">
+                            Sign In
+                          </button>
+                        </div>
+
+                        <div class="mt-4 text-center">
+                          <div class="signin-other-title">
+                            <h5 class="fs-13 mb-4 title">Sign In with</h5>
+                          </div>
+
+                          <div>
+                            <button type="button" class="btn btn-primary btn-icon waves-effect waves-light">
+                              <em class="ri-facebook-fill fs-16"></em>
+                            </button>
+                            <button type="button" class="btn btn-danger btn-icon waves-effect waves-light ms-1">
+                              <em class="ri-google-fill fs-16"></em>
+                            </button>
+                            <button type="button" class="btn btn-dark btn-icon waves-effect waves-light ms-1">
+                              <em class="ri-github-fill fs-16"></em>
+                            </button>
+                            <button type="button" class="btn btn-info btn-icon waves-effect waves-light ms-1">
+                              <em class="ri-twitter-fill fs-16"></em>
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+
+                    <div class="mt-5 text-center">
+                      <p class="mb-0">
+                        Don't have an account ?
+                        <router-link to="/auth/signup-cover" class="fw-semibold text-primary text-decoration-underline">
+                          Signup</router-link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- end col -->
               </div>
-              <!-- end card body -->
+              <!-- end row -->
             </div>
             <!-- end card -->
-
-            <div class="mt-4 text-center">
-              <p class="mb-0">
-                Already have an account ?
-                <router-link to="/auth/signin-basic" class="fw-semibold text-primary text-decoration-underline">
-                  Signin
-                </router-link>
-              </p>
-            </div>
           </div>
+          <!-- end col -->
         </div>
         <!-- end row -->
       </div>
@@ -321,7 +129,7 @@ export default {
         <div class="row">
           <div class="col-lg-12">
             <div class="text-center">
-              <p class="mb-0 text-muted">
+              <p class="mb-0">
                 &copy; {{ new Date().getFullYear() }} MedicFollow. Crafted with
                 <em class="mdi mdi-heart text-danger"></em> by MedicFollow
               </p>
