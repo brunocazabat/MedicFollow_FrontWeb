f3749bd2b23895572e1c032a434f9bb2ca30a497
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-22 02:16:14 +0200

    Mise en place de Components pour alleger le code

---
+++ src/views/log-account-section/components/admin-panel-log.vue
@@ -0,0 +1,91 @@
+<script>
+export default {
+  name: "adminmodule",
+
+  methods: {
+    setadmin() {
+      if (process.env.VUE_APP_DEFAULT_AUTH === "DEV") {
+        console.log(process.env.VUE_APP_DEFAULT_AUTH)
+        return true;
+      } else {
+        return false;
+      }
+    },
+  }
+};
+</script>
+
+<template>
+  <div class="col-md-8 col-lg-6 col-xl-2" v-if="setadmin()">
+    <div class="card mt-4 p-4">
+      TEST
+      <ul>
+        <li>
+          <router-link to="/test">
+            /test
+          </router-link>
+        </li>
+      </ul>
+      LOG IN OUT
+      <ul>
+        <li>
+          <router-link to="/login">
+            /login
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/logout">
+            /logout
+          </router-link>
+        </li>
+      </ul>
+      FORGET PASSWORD
+      <ul>
+        <li>
+          <router-link to="/forgot-password">
+            /fgt-pwd
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/forgot-password-success">
+            /fgt-pwd-success
+          </router-link>
+        </li>
+      </ul>
+      REGISTER MAIL
+      <ul>
+        <li>
+          <router-link to="/register-mail">
+            /rgst-@
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/register-mail-success">
+            /rgst-@-success
+          </router-link>
+        </li>
+      </ul>
+      REGISTER INFO
+      <ul>
+        <li>
+          <router-link to="/register-fill">
+            /rgst-$
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/register-fill-success">
+            /rgst-$-success
+          </router-link>
+        </li>
+      </ul>
+      LOCKSCREEN
+      <ul>
+        <li>
+          <router-link to="/lockscreen">
+            /lockscreen
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ src/views/log-account-section/components/logo-header-module.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "logoheadermodule",
+};
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="text-center mt-sm-5 mb-4 text-white-50">
+        <div>
+          <router-link to="/" class="d-inline-block auth-logo">
+            <img src="@/assets/images/logo/logo-long.png" alt="" height="120" />
+          </router-link>
+        </div>
+        <p class="mt-3 fs-15 fw-medium" style="color: white" data-key="t-yourtool">{{ $t("t-yourtool") }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
