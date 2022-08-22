8cf22f68ff7c31d5c3b2e5d2621c7adafec9dbb7
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-22 21:45:47 +0200

    Create footer-log-module.vue

---
+++ src/views/log-account-section/components/footer-log-module.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "footerlogmodule",
+};
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="text-center">
+            <p class="mb-0 text-muted">
+              &copy; {{ new Date().getFullYear() }} MedicFollow. Crafted with
+              <em class="mdi mdi-heart text-danger"></em> by MedicFollow
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
