3a4265b728707e162336110bb7894338b9807c7f
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-22 02:16:49 +0200

    Injection des modules dans forgot password

--- src/views/log-account-section/forgot-password.vue
+++ src/views/log-account-section/forgot-password.vue
@@ -1,14 +1,16 @@
 <script>
 import { authMethods } from "@/components/state/helpers";
 import appConfig from "../../../app.config";
-
 import { required, email, helpers } from "@vuelidate/validators";
 import useVuelidate from "@vuelidate/core";
 import Lottie from "@/components/widgets/lottie.vue";
 import animationData from "@/assets/anim/animation2.json";
+import translatemodule from "./components/translate-module.vue";
+import logoheadermodule from "./components/logo-header-module.vue";
+import particlesmodule from "./components/particles-module.vue";
 
 export default {
-  components: { lottie: Lottie },
+  components: { lottie: Lottie, translatemodule, logoheadermodule, particlesmodule },
   setup() {
     return { v$: useVuelidate() };
   },
@@ -78,167 +80,16 @@ export default {
 <template>
   <div class="auth-page-wrapper pt-5">
     <!-- auth page bg -->
-    <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-      <div class="bg-overlay"></div>
-
-      <div class="shape">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 1440 120"
-        >
-          <path
-            d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"
-          ></path>
-        </svg>
-      </div>
-      <Particles
-        id="tsparticles"
-        :options="{
-          particles: {
-            number: {
-              value: 90,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: '#ffffff',
-            },
-            shape: {
-              type: 'circle',
-              stroke: {
-                width: 0,
-                color: '#000000',
-              },
-              polygon: {
-                nb_sides: 5,
-              },
-              image: {
-                src: 'img/github.svg',
-                width: 100,
-                height: 100,
-              },
-            },
-            opacity: {
-              value: 0.8,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0,
-                sync: false,
-              },
-            },
-            size: {
-              value: 4,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 4,
-                size_min: 0.2,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: false,
-              distance: 150,
-              color: '#ffffff',
-              opacity: 0.4,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-              direction: 'none',
-              random: false,
-              straight: false,
-              out_mode: 'out',
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          },
-          interactivity: {
-            detect_on: 'canvas',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'bubble',
-              },
-              onclick: {
-                enable: true,
-                mode: 'repulse',
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 4,
-                duration: 2,
-                opacity: 0.8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 200,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
-          retina_detect: true,
-          config_demo: {
-            hide_card: false,
-            background_color: '#b61924',
-            background_image: '',
-            background_position: '50% 50%',
-            background_repeat: 'no-repeat',
-            background_size: 'cover',
-          },
-        }"
-      />
-    </div>
+    <particlesmodule />
 
     <!-- auth page content -->
     <div class="auth-page-content">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-center mt-sm-5 mb-4 text-white-50">
-              <div>
-                <router-link to="/" class="d-inline-block auth-logo">
-                  <img
-                    src="@/assets/images/logo/logo-long.png"
-                    alt=""
-                    height="120"
-                  />
-                </router-link>
-              </div>
-              <p class="mt-3 fs-15 fw-medium" style="color: white">
-                Votre outil de communication avec vos proches
-              </p>
-            </div>
-          </div>
-        </div>
+        <logoheadermodule />
         <!-- end row -->
 
         <div class="row justify-content-center">
-          <div class="col-md-8 col-lg-6 col-xl-5">
+          <div class="col-md-8 col-lg-6 col-xl-6">
             <div class="card mt-4">
               <div class="card-body p-4">
                 <div class="text-center mt-2">
@@ -246,41 +97,18 @@ export default {
                     We will send an email to your address to reset your
                     password.
                   </p>
-                  <lottie
-                    class="avatar-xl"
-                    colors="primary:#45CB85,secondary:#4b38b3"
-                    :options="defaultOptions"
-                    :height="120"
-                    :width="120"
-                  />
+                  <lottie class="avatar-xl" colors="primary:#45CB85,secondary:#4b38b3" :options="defaultOptions"
+                    :height="120" :width="120" />
                 </div>
 
                 <div class="p-2">
-                  <b-alert
-                    v-model="isResetError"
-                    class="mb-4"
-                    variant="danger"
-                    dismissible
-                    >{{ error }}</b-alert
-                  >
+                  <b-alert v-model="isResetError" class="mb-4" variant="danger" dismissible>{{ error }}</b-alert>
                   <form @submit.prevent="tryToReset">
                     <div class="mb-4">
-                      <label class="form-label"
-                        >Email <span class="text-danger">*</span></label
-                      >
-                      <input
-                        type="email"
-                        v-model="email"
-                        class="form-control"
-                        id="email"
-                        :class="{ 'is-invalid': submitted && v$.email.$errors }"
-                        placeholder="Enter Email"
-                      />
-                      <div
-                        v-for="(item, index) in v$.email.$errors"
-                        :key="index"
-                        class="invalid-feedback"
-                      >
+                      <label class="form-label">Email <span class="text-danger">*</span></label>
+                      <input type="email" v-model="email" class="form-control" id="email"
+                        :class="{ 'is-invalid': submitted && v$.email.$errors }" placeholder="Enter Email" />
+                      <div v-for="(item, index) in v$.email.$errors" :key="index" class="invalid-feedback">
                         <span v-if="item.$message">{{ item.$message }}</span>
                       </div>
                     </div>
@@ -293,10 +121,7 @@ export default {
                     <div class="mt-4 text-center">
                       <p class="mb-0 text-muted" style="color: black">
                         Wait, I remember my password.
-                        <router-link
-                          to="/login"
-                          class="fw-semibold text-primary text-decoration-underline"
-                        >
+                        <router-link to="/login" class="fw-semibold text-primary text-decoration-underline">
                           Signin
                         </router-link>
                       </p>
@@ -309,6 +134,7 @@ export default {
             </div>
             <!-- end card -->
           </div>
+          <translatemodule />
         </div>
         <!-- end row -->
       </div>
