172847a0a3f97bec6d52f0064a919b96a7a3f6d0
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-22 21:46:59 +0200

    Update forgot-password-success.vue

--- src/views/log-account-section/forgot-password-success.vue
+++ src/views/log-account-section/forgot-password-success.vue
@@ -58,20 +58,7 @@ export default {
     <!-- end auth page content -->
 
     <!-- footer -->
-    <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-center">
-              <p class="mb-0 text-muted">
-                &copy; {{ new Date().getFullYear() }} MedicFollow. Crafted with
-                <em class="mdi mdi-heart text-danger"></em> by MedicFollow
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <footerlogmodule />
     <!-- end Footer -->
   </div>
   <!-- end auth-page-wrapper -->
