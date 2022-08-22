eef79ec783163ae00f0ff44146a434e8b98ff683
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-22 02:17:19 +0200

    Injection des modules dans register fill

--- src/views/log-account-section/register-fill.vue
+++ src/views/log-account-section/register-fill.vue
@@ -3,13 +3,10 @@ import { required, helpers } from "@vuelidate/validators";
 import useVuelidate from "@vuelidate/core";
 import { mapState } from "vuex";
 import Popper from "vue3-popper";
-
-import {
-  authMethods,
-  authFackMethods,
-  notificationMethods,
-} from "@/components/state/helpers";
-
+import translatemodule from "./components/translate-module.vue";
+import logoheadermodule from "./components/logo-header-module.vue";
+import particlesmodule from "./components/particles-module.vue";
+import { authMethods, authFackMethods, notificationMethods } from "@/components/state/helpers";
 import appConfig from "../../../app.config";
 
 export default {
@@ -25,9 +22,7 @@ export default {
       },
     ],
   },
-  components: {
-    Popper,
-  },
+  components: { Popper, translatemodule, logoheadermodule, particlesmodule },
   data() {
     return {
       user: {
@@ -70,6 +65,7 @@ export default {
     tryToRegisterIn() {
       this.submitted = true;
       this.v$.$touch();
+      this.$router.push("/register-fill-success");
     },
     toggleShow() {
       this.showPassword = !this.showPassword;
@@ -87,167 +83,16 @@ export default {
 <template>
   <div class="auth-page-wrapper pt-5">
     <!-- auth page bg -->
-    <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-      <div class="bg-overlay"></div>
-
-      <div class="shape">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 1440 120"
-        >
-          <path
-            d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"
-          ></path>
-        </svg>
-      </div>
-      <Particles
-        id="tsparticles"
-        :options="{
-          particles: {
-            number: {
-              value: 90,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: '#ffffff',
-            },
-            shape: {
-              type: 'circle',
-              stroke: {
-                width: 0,
-                color: '#000000',
-              },
-              polygon: {
-                nb_sides: 5,
-              },
-              image: {
-                src: 'img/github.svg',
-                width: 100,
-                height: 100,
-              },
-            },
-            opacity: {
-              value: 0.8,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0,
-                sync: false,
-              },
-            },
-            size: {
-              value: 4,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 4,
-                size_min: 0.2,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: false,
-              distance: 150,
-              color: '#ffffff',
-              opacity: 0.4,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-              direction: 'none',
-              random: false,
-              straight: false,
-              out_mode: 'out',
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          },
-          interactivity: {
-            detect_on: 'canvas',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'bubble',
-              },
-              onclick: {
-                enable: true,
-                mode: 'repulse',
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 4,
-                duration: 2,
-                opacity: 0.8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 200,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
-          retina_detect: true,
-          config_demo: {
-            hide_card: false,
-            background_color: '#b61924',
-            background_image: '',
-            background_position: '50% 50%',
-            background_repeat: 'no-repeat',
-            background_size: 'cover',
-          },
-        }"
-      />
-    </div>
+    <particlesmodule />
 
     <!-- auth page content -->
     <div class="auth-page-content">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-center mt-sm-5 mb-4 text-white-50">
-              <div>
-                <router-link to="/" class="d-inline-block auth-logo">
-                  <img
-                    src="@/assets/images/logo/logo-long.png"
-                    alt=""
-                    height="120"
-                  />
-                </router-link>
-              </div>
-              <p class="mt-3 fs-15 fw-medium" style="color: white">
-                Votre outil de communication avec vos proches
-              </p>
-            </div>
-          </div>
-        </div>
+        <logoheadermodule />
         <!-- end row -->
 
         <div class="row justify-content-center">
-          <div class="col-md-8 col-lg-6 col-xl-5">
+          <div class="col-md-8 col-lg-6 col-xl-6">
             <div class="card mt-4">
               <div class="card-body p-4">
                 <div class="text-center">
@@ -256,94 +101,47 @@ export default {
                   </p>
                 </div>
                 <div class="p-2 mt-4">
-                  <form
-                    class="needs-validation"
-                    @submit.prevent="tryToRegisterIn"
-                  >
-                    <b-alert
-                      v-model="registerSuccess"
-                      class="mt-3"
-                      variant="success"
-                      dismissible
-                      >Registration successfull.</b-alert
-                    >
+                  <form class="needs-validation" @submit.prevent="tryToRegisterIn">
+                    <b-alert v-model="registerSuccess" class="mt-3" variant="success" dismissible>Registration
+                      successfull.</b-alert>
 
-                    <b-alert
-                      v-model="isRegisterError"
-                      class="mt-3"
-                      variant="danger"
-                      dismissible
-                      >{{ regError }}
+                    <b-alert v-model="isRegisterError" class="mt-3" variant="danger" dismissible>{{ regError }}
                     </b-alert>
 
-                    <div
-                      v-if="notification.message"
-                      :class="'alert ' + notification.type"
-                    >
+                    <div v-if="notification.message" :class="'alert ' + notification.type">
                       {{ notification.message }}
                     </div>
 
                     <!-- Familly Name Input row -->
                     <div class="mb-3">
-                      <label for="famillyname" class="form-label"
-                        >Familly name <span class="text-danger">*</span></label
-                      >
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="user.famillyname"
-                        onpaste="return false"
-                        :class="{
-                          'is-invalid': submitted && v$.user.famillyname.$error,
-                        }"
-                        id="famillyname"
-                        placeholder="Enter familly name"
-                        required
-                      />
-                      <div
-                        v-if="submitted && v$.user.famillyname.$error"
-                        class="invalid-feedback"
-                      >
+                      <label for="famillyname" class="form-label">Familly name <span
+                          class="text-danger">*</span></label>
+                      <input type="text" class="form-control" v-model="user.famillyname" onpaste="return false" :class="{
+                        'is-invalid': submitted && v$.user.famillyname.$error,
+                      }" id="famillyname" placeholder="Enter familly name" required />
+                      <div v-if="submitted && v$.user.famillyname.$error" class="invalid-feedback">
                         <span v-if="v$.user.famillyname.required.$message">{{
-                          v$.user.famillyname.required.$message
+                            v$.user.famillyname.required.$message
                         }}</span>
                       </div>
                     </div>
 
                     <!-- Name Input row -->
                     <div class="mb-3">
-                      <label for="name" class="form-label"
-                        >Name <span class="text-danger">*</span></label
-                      >
-                      <input
-                        type="text"
-                        class="form-control"
-                        v-model="user.name"
-                        onpaste="return false"
-                        :class="{
-                          'is-invalid': submitted && v$.user.name.$error,
-                        }"
-                        id="name"
-                        placeholder="Enter name"
-                        required
-                      />
-                      <div
-                        v-if="submitted && v$.user.name.$error"
-                        class="invalid-feedback"
-                      >
+                      <label for="name" class="form-label">Name <span class="text-danger">*</span></label>
+                      <input type="text" class="form-control" v-model="user.name" onpaste="return false" :class="{
+                        'is-invalid': submitted && v$.user.name.$error,
+                      }" id="name" placeholder="Enter name" required />
+                      <div v-if="submitted && v$.user.name.$error" class="invalid-feedback">
                         <span v-if="v$.user.name.required.$message">{{
-                          v$.user.name.required.$message
+                            v$.user.name.required.$message
                         }}</span>
                       </div>
                     </div>
 
                     <!-- Password Input row -->
                     <div class="mb-3">
-                      <div
-                        class="float-end"
-                        v-on:mouseover="mouseover"
-                        v-on:mouseleave="mouseleave"
-                      >
+                      <div class="float-end" v-on:mouseover="mouseover" v-on:mouseleave="mouseleave">
                         <Popper placement="right" :show="hover">
                           <p class="text-danger">Password requierements</p>
                           <template #content>
@@ -366,87 +164,50 @@ export default {
                           </template>
                         </Popper>
                       </div>
-                      <label class="form-label" for="password-input"
-                        >Password <span class="text-danger">*</span></label
-                      >
+                      <label class="form-label" for="password-input">Password <span class="text-danger">*</span></label>
                       <div class="position-relative auth-pass-inputgroup mb-3">
-                        <input
-                          v-if="showPassword"
-                          type="text"
-                          v-model="user.password"
-                          onpaste="return false"
-                          class="form-control pe-5"
-                          :class="{
+                        <input v-if="showPassword" type="text" v-model="user.password" onpaste="return false"
+                          class="form-control pe-5" :class="{
                             'is-invalid': submitted && v$.user.password.$error,
-                          }"
-                          placeholder="Enter password"
-                          id="password-input"
-                        />
-                        <input
-                          v-else
-                          type="password"
-                          v-model="user.password"
-                          onpaste="return false"
-                          class="form-control pe-5"
-                          :class="{
+                          }" placeholder="Enter password" id="password-input" />
+                        <input v-else type="password" v-model="user.password" onpaste="return false"
+                          class="form-control pe-5" :class="{
                             'is-invalid': submitted && v$.user.password.$error,
-                          }"
-                          placeholder="Enter password"
-                          id="password-input"
-                        />
-                        <button
-                          @click="toggleShow"
+                          }" placeholder="Enter password" id="password-input" />
+                        <button @click="toggleShow"
                           class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted"
-                          type="button"
-                          style="box-shadow: none !important"
-                          id="password-addon"
-                        >
+                          type="button" style="box-shadow: none !important" id="password-addon">
                           <em class="ri-eye-fill align-middle"></em>
                         </button>
-                        <div
-                          v-if="submitted && v$.user.password.$error"
-                          class="invalid-feedback"
-                        >
+                        <div v-if="submitted && v$.user.password.$error" class="invalid-feedback">
                           <span v-if="v$.user.password.required.$message">{{
-                            v$.user.password.required.$message
+                              v$.user.password.required.$message
                           }}</span>
                         </div>
                       </div>
                     </div>
 
                     <div class="form-check">
-                      <input
-                        class="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="auth-remember-check"
-                        required
-                      />
-                      <label class="form-check-label" for="auth-remember-check"
-                        ><p class="mb-0 fs-12 text-muted fst-italic">
+                      <input class="form-check-input" type="checkbox" value="" id="auth-remember-check" required />
+                      <label class="form-check-label" for="auth-remember-check">
+                        <p class="mb-0 fs-12 text-muted fst-italic">
                           By registering you agree to the MedicFollow
-                          <a
-                            href="https://medicfollow.fr/fr/usage.html"
-                            target="_blank"
+                          <a href="https://medicfollow.fr/fr/usage.html" target="_blank"
                             class="text-primary text-decoration-underline fst-normal fw-medium"
-                            rel="noreferrer noopener"
-                            >Terms of use
+                            rel="noreferrer noopener">Terms of use
                           </a>
-                        </p></label
-                      >
+                        </p>
+                      </label>
                     </div>
 
                     <div class="mt-4">
-                      <button class="btn btn-success w-100" type="submit">
+                      <button @click="tryToRegisterIn" class="btn btn-success w-100" type="submit">
                         Sign Up
                       </button>
                       <div class="mt-4 text-center">
                         <p class="mb-0 text-muted" style="color: black">
                           Already have an account ?
-                          <router-link
-                            to="/login"
-                            class="fw-semibold text-primary text-decoration-underline"
-                          >
+                          <router-link to="/login" class="fw-semibold text-primary text-decoration-underline">
                             Signin
                           </router-link>
                         </p>
@@ -459,6 +220,7 @@ export default {
             </div>
             <!-- end card -->
           </div>
+          <translatemodule />
         </div>
         <!-- end row -->
       </div>
