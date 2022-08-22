411a1a7bb11af0aa65a736ec94f782cc758b6ef1
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-22 02:17:07 +0200

    Injection des modules dans login logout

--- src/views/log-account-section/logout.vue
+++ src/views/log-account-section/logout.vue
@@ -1,6 +1,9 @@
 <script>
 import animationData from "@/assets/anim/animation1.json";
 import Lottie from "@/components/widgets/lottie.vue";
+import translatemodule from "./components/translate-module.vue";
+import logoheadermodule from "./components/logo-header-module.vue";
+import particlesmodule from "./components/particles-module.vue";
 
 export default {
   data() {
@@ -10,181 +13,27 @@ export default {
       },
     };
   },
-  components: {
-    lottie: Lottie,
-  },
+  components: { lottie: Lottie, translatemodule, logoheadermodule, particlesmodule },
 };
 </script>
 
 <template>
   <div class="auth-page-wrapper pt-5">
     <!-- auth page bg -->
-    <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-      <div class="bg-overlay"></div>
-
-      <div class="shape">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 1440 120"
-        >
-          <path
-            d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"
-          ></path>
-        </svg>
-      </div>
-      <Particles
-        id="tsparticles"
-        :options="{
-          particles: {
-            number: {
-              value: 90,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: '#ffffff',
-            },
-            shape: {
-              type: 'circle',
-              stroke: {
-                width: 0,
-                color: '#000000',
-              },
-              polygon: {
-                nb_sides: 5,
-              },
-              image: {
-                src: 'img/github.svg',
-                width: 100,
-                height: 100,
-              },
-            },
-            opacity: {
-              value: 0.8,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0,
-                sync: false,
-              },
-            },
-            size: {
-              value: 4,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 4,
-                size_min: 0.2,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: false,
-              distance: 150,
-              color: '#ffffff',
-              opacity: 0.4,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-              direction: 'none',
-              random: false,
-              straight: false,
-              out_mode: 'out',
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          },
-          interactivity: {
-            detect_on: 'canvas',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'bubble',
-              },
-              onclick: {
-                enable: true,
-                mode: 'repulse',
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 4,
-                duration: 2,
-                opacity: 0.8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 200,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
-          retina_detect: true,
-          config_demo: {
-            hide_card: false,
-            background_color: '#b61924',
-            background_image: '',
-            background_position: '50% 50%',
-            background_repeat: 'no-repeat',
-            background_size: 'cover',
-          },
-        }"
-      />
-    </div>
+    <particlesmodule />
 
     <!-- auth page content -->
     <div class="auth-page-content">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-center mt-sm-5 mb-4 text-white-50">
-              <div>
-                <router-link to="/" class="d-inline-block auth-logo">
-                  <img
-                    src="@/assets/images/logo/logo-long.png"
-                    alt=""
-                    height="20"
-                  />
-                </router-link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <logoheadermodule />
         <!-- end row -->
 
         <div class="row justify-content-center">
-          <div class="col-md-8 col-lg-6 col-xl-5">
+          <div class="col-md-8 col-lg-6 col-xl-6">
             <div class="card mt-4">
               <div class="card-body p-4 text-center">
-                <lottie
-                  colors="primary:#4b38b3,secondary:#08a88a"
-                  :options="defaultOptions"
-                  :height="180"
-                  :width="180"
-                />
+                <lottie colors="primary:#4b38b3,secondary:#08a88a" :options="defaultOptions" :height="180"
+                  :width="180" />
                 <div class="mt-4 pt-2">
                   <h5>You are Logged Out</h5>
                   <p class="text-muted">
@@ -192,8 +41,7 @@ export default {
                     <span class="fw-semibold">MedicFollow</span>
                   </p>
                   <div class="mt-4">
-                    <router-link to="/login" class="btn btn-success w-100"
-                      >Sign In
+                    <router-link to="/login" class="btn btn-success w-100">Sign In
                     </router-link>
                   </div>
                 </div>
@@ -202,6 +50,7 @@ export default {
             </div>
             <!-- end card -->
           </div>
+          <translatemodule />
           <!-- end col -->
         </div>
         <!-- end row -->
