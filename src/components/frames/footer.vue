f50a5da063431f4df46f9a82ee5d15f866d3a2e8
bruno cazabat <bruno.cazabat@epitech.eu>
2022-08-27 23:32:08 +0200

    Changement adaptatif du footer une fois log

--- src/components/frames/footer.vue
+++ src/components/frames/footer.vue
@@ -1,16 +1,12 @@
+<script>
+import footerlogmodule from "../login-components/footer-log-module.vue";
+
+export default {
+  name: "footer",
+  components: { footerlogmodule },
+}
+</script>
+
 <template>
-  <footer class="footer">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-6">
-          {{ new Date().getFullYear() }} © MedicFollow.
-        </div>
-        <div class="col-sm-6">
-          <div class="text-sm-end d-none d-sm-block">
-            Designed & Developed by MedicFollow
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <footerlogmodule />
 </template>
