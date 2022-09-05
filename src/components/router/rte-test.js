a5dfaf86ea8bd6de9767c1e82ae285e2831746b7
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-05 20:38:57 +0200

    Create rte-test.js

---
+++ src/components/router/rte-test.js
@@ -0,0 +1,17 @@
+//let mode = localStorage.getItem("mode");
+
+export default [
+  // test routes START
+  /*
+  {
+    path: "/test",
+    name: "test",
+    component: () => import("@/views/dashboard-section/index.vue"),
+    meta: {
+      title: "Test",
+      authRequired: mode,
+    },
+  },
+  */
+  // test routes END
+];
