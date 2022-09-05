c0589e261c5f099605060f036e5e10db9ff255af
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-05 20:38:54 +0200

    Create rte-login.js

---
+++ src/components/router/rte-login.js
@@ -0,0 +1,98 @@
+import store from "@/components/state/store";
+
+let mode = localStorage.getItem("mode");
+
+function onceLoggedIn(_routeTo, _routeFrom, next) {
+  // If the user is already logged in
+  if (store.getters["auth/isloggedIn"] && !store.getters["auth/isLocked"]) {
+    // Redirect to the home page instead
+    next({ name: "default" });
+  } else if (store.getters["auth/isLocked"]) {
+    // Redirect to the lockscreen page instead
+    next({ name: "lockscreen" });
+  } else {
+    // Continue to the login page
+    next();
+  }
+}
+
+export default [
+  {
+    // Logout is not protected by onceLoggedIn mais par authRequired > pas d'accès si pas connecté
+    path: "/logout",
+    name: "logout",
+    component: () => import("@/views/log-account-section/logout.vue"),
+    meta: {
+      title: "Logout",
+      authRequired: mode,
+    },
+  },
+  // log-account-section routes START
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/log-account-section/login.vue"),
+    meta: {
+      title: "Login",
+      onceLoggedIn,
+    },
+  },
+  {
+    path: "/register-mail",
+    name: "register-mail",
+    component: () => import("@/views/log-account-section/register-mail.vue"),
+    meta: {
+      title: "Enter your email",
+      onceLoggedIn,
+    },
+  },
+  {
+    path: "/register-mail-success",
+    name: "mail-success",
+    component: () =>
+      import("@/views/log-account-section/register-mail-success.vue"),
+    meta: {
+      title: "Success",
+      onceLoggedIn,
+    },
+  },
+  {
+    path: "/register-fill",
+    name: "register-fill",
+    component: () => import("@/views/log-account-section/register-fill.vue"),
+    meta: {
+      title: "Register your account",
+      onceLoggedIn,
+    },
+  },
+  {
+    path: "/register-fill-success",
+    name: "fill-success",
+    component: () =>
+      import("@/views/log-account-section/register-fill-success.vue"),
+    meta: {
+      title: "Success",
+      onceLoggedIn,
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "forgot-password",
+    component: () => import("@/views/log-account-section/forgot-password.vue"),
+    meta: {
+      title: "Forgot Password",
+      onceLoggedIn,
+    },
+  },
+  {
+    path: "/forgot-password-success",
+    name: "forgot-password-success",
+    component: () =>
+      import("@/views/log-account-section/forgot-password-success.vue"),
+    meta: {
+      title: "Success",
+      onceLoggedIn,
+    },
+  },
+  // log-account-section routes END
+];
