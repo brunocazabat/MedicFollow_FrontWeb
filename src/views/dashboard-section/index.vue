f4cbc6f3b3eb028ec9a14efc437f13fc476c65c5
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-06 16:02:34 +0200

    Update index.vue

--- src/views/dashboard-section/index.vue
+++ src/views/dashboard-section/index.vue
@@ -1,6 +1,5 @@
 <script>
 import Layout from "@/components/layouts/main.vue";
-import PageHeader from "@/components/frames/page-header";
 import appConfig from "@/../app.config";
 
 import Widgets from "../dashboard-components-section/widget.vue";
@@ -12,23 +11,8 @@ export default {
     title: "Projects",
     meta: [{ name: "description", content: appConfig.description }],
   },
-  data() {
-    return {
-      title: "Projects",
-      items: [
-        {
-          text: "Connected as",
-        },
-        {
-          text: "Dev",
-          active: true,
-        },
-      ],
-    }
-  },
   components: {
     Layout,
-    PageHeader,
     Widgets,
     Schedules,
     Chat
@@ -38,21 +22,18 @@ export default {
 
 <template>
   <Layout>
-    <PageHeader :title="title" :items="items" />
-
     <div class="row project-wrapper">
-      <div class="col-xxl-8">
+      <div class="col-xxl-3">
         <div class="row">
           <Widgets />
         </div>
+      </div>
+      <div class="col-xxl-4">
         <div class="row">
-          <div class="col-xl-12">
-            <Chat />
-          </div>
+          <Chat />
         </div>
       </div>
-
-      <div class="col-xxl-4">
+      <div class="col-xxl-5">
         <Schedules />
       </div>
     </div>
