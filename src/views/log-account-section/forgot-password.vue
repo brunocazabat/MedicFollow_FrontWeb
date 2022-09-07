b35ac81ba638ea251f996497a904db5f3f60aff1
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-07 20:18:53 +0200

    Captcha en place forget password

--- src/views/log-account-section/forgot-password.vue
+++ src/views/log-account-section/forgot-password.vue
@@ -1,4 +1,5 @@
 <script>
+import { mapActions, mapGetters } from "vuex";
 import { authMethods } from "@/components/state/helpers";
 import appConfig from "@/../app.config";
 import { required, email, helpers } from "@vuelidate/validators";
@@ -9,9 +10,10 @@ import translatemodule from "@/components/login-components/translate-module.vue"
 import logoheadermodule from "@/components/login-components/logo-header-module.vue";
 import particlesmodule from "@/components/login-components/particles-module.vue";
 import footermodule from "@/components/login-components/footer-module.vue";
+import recaptcha from "@/components/widgets/recaptchav2.vue";
 
 export default {
-  components: { lottie: Lottie, translatemodule, logoheadermodule, particlesmodule, footermodule },
+  components: { lottie: Lottie, translatemodule, logoheadermodule, particlesmodule, footermodule, recaptcha },
   setup() {
     return { v$: useVuelidate() };
   },
@@ -34,25 +36,39 @@ export default {
     return {
       email: "",
       submitted: false,
-      error: null,
+      ResetError: null,
       tryingToReset: false,
       isResetError: false,
       defaultOptions: { animationData: animationData },
     };
   },
   methods: {
+    ...mapActions({
+      setCaptchaValid: "security/setCaptchaValid",
+    }),
+    ...mapGetters({
+      isRecaptchaEnabled: "security/isRecaptchaEnabled",
+    }),
     ...authMethods,
-    // Try to register the user in with the email, fullname
-    // and password they provided.
     tryToReset() {
       this.submitted = true;
-      // stop here if form is invalid
       this.v$.$touch();
 
-      if (this.v$.$invalid) {
-        return;
+      if (!this.v$.$invalid && this.isRecaptchaEnabled()) {
+        this.isResetError = false;
+        this.ResetError = null;
+        this.submitted = false;
+        this.setCaptchaValid(false);
+        this.$router.push(
+          this.$route.query.redirectFrom || {
+            path: "/forgot-password-success",
+          }
+        );
+      } else {
+        this.submitted = false;
+        this.isResetError = true;
+        this.ResetError = "Please complete the captcha and all fields.";
       }
-
     },
   },
 };
@@ -74,34 +90,36 @@ export default {
             <div class="card mt-4">
               <div class="card-body p-4">
                 <div class="text-center mt-2">
-                  <p class="text-muted" data-key="t-willreset">{{  $t("t-willreset")  }}
+                  <p class="text-muted" data-key="t-willreset">{{ $t("t-willreset") }}
                   </p>
                   <lottie class="avatar-xl" colors="primary:#45CB85,secondary:#4b38b3" :options="defaultOptions"
                     :height="120" :width="120" />
                 </div>
 
                 <div class="p-2">
-                  <b-alert v-model="isResetError" class="mb-4" variant="danger" dismissible>{{  error  }}</b-alert>
+                  <b-alert v-model="isResetError" class="mb-4" variant="danger" dismissible>{{ ResetError }}</b-alert>
                   <form @submit.prevent="tryToReset">
                     <div class="mb-4">
-                      <label for="email" class="form-label" data-key="t-email">{{  $t("t-email")  }} <span
+                      <label for="email" class="form-label" data-key="t-email">{{ $t("t-email") }} <span
                           class="text-danger">*</span></label>
                       <input type="email" v-model="email" class="form-control" id="email"
                         :class="{ 'is-invalid': submitted && v$.email.$errors }" data-key="t-entermail"
                         v-bind:placeholder="$t('t-entermail')" />
                       <div v-for="(item, index) in v$.email.$errors" :key="index" class="invalid-feedback">
-                        <span v-if="item.$message">{{  item.$message  }}</span>
+                        <span v-if="item.$message">{{ item.$message }}</span>
                       </div>
                     </div>
 
+                    <recaptcha />
                     <div class="text-center mt-4">
-                      <button class="btn btn-success w-100" type="submit" data-key="t-resetlink">{{  $t("t-resetlink")  }}
+                      <button @click="tryToReset" class="btn btn-success w-100" type="submit" data-key="t-resetlink">{{
+                      $t("t-resetlink") }}
                       </button>
                     </div>
                     <div class="mt-4 text-center">
-                      <p class="mb-0 text-muted" style="color: black" data-key="t-remempass">{{  $t("t-remempass")  }}
+                      <p class="mb-0 text-muted" style="color: black" data-key="t-remempass">{{ $t("t-remempass") }}
                         <router-link to="/login" class="fw-semibold text-primary text-decoration-underline"
-                          data-key="t-signin">{{  $t("t-signin")  }}
+                          data-key="t-signin">{{ $t("t-signin") }}
                         </router-link>
                       </p>
                     </div>
