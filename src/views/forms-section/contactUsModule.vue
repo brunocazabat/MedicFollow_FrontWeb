22774d39d952f7a38214893f7c621346e23a2676
Simon9991 <simon.bernabeu@epitech.eu>
2022-09-17 17:42:14 +0200

    Changed "Contact Us" to Cards
    Not raw text anymore, they're cards

--- src/views/forms-section/contactUsModule.vue
+++ src/views/forms-section/contactUsModule.vue
@@ -10,13 +10,41 @@ export default {
 
 
 <template>
-  <div class="p-3 mb-3">
-    <h3 class="mb-1 mt-0">Email:</h3>
-    <p class="p-3 font-size-medium">contact@medicfollow.fr</p>
+
+  <div class="p-2">
+
+
+    <!-- EMAIL ADDRESS   -->
+    <div class="col-xl-4 mb-5">
+      <div class="card">
+        <div class="card-header">
+          <em class="float-end text-primary me-2 ri-mail-line ri-2x"></em>
+          <h3 class="card-title font-size-x-large">Our Email address:</h3>
+          <p class="card-subtitle text-muted mb-2">We usually take few days to answer emails.</p>
+        </div>
+        <div class="card-body">
+          <p class="font-size-medium">
+            contact@medicfollow.fr
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- PHONE NUMBER -->
+    <div class="col-xl-4">
+      <div class="card">
+        <div class="card-header">
+          <em class="float-end text-primary me-2 ri-phone-line ri-2x"></em>
+          <h3 class="card-title font-size-x-large">Our Phone number <strong>(SMS ONLY)</strong>:</h3>
+          <p class="card-subtitle text-muted mb-2">We will only answer to SMS!</p>
+        </div>
+        <div class="card-body">
+          <p class="font-size-medium">
+            +33 7 64 85 22 23
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <div class="p-3 mb-3">
-    <h3 class="mb-1 mt-0">Phone Number <strong>(SMS ONLY)</strong>:</h3>
-    <p class="p-3 font-size-medium">+33 7 64 85 22 23</p>
-  </div>
 </template>
