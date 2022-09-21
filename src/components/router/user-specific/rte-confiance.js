1219d01394f1002900b6907d55bcebe001040777
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-21 16:38:52 +0200

    Create rte-confiance.js

---
+++ src/components/router/user-specific/rte-confiance.js
@@ -0,0 +1,111 @@
+import store from "@/components/state/store";
+
+function onceLoggedIn(_routeTo, _routeFrom, next) {
+  if (
+    _routeTo.path === "/" &&
+    store.getters["auth/getisloggedIn"] &&
+    !store.getters["security/getisLocked"] &&
+    (store.getters["auth/getuserType"] === "confiance" ||
+      store.getters["auth/getuserType"] === "admin")
+  ) {
+    next({ path: "/confiance/dashboard" });
+  } else if (store.getters["security/getisLocked"]) {
+    // Redirect to the lockscreen page instead
+    next({ path: "/lockscreen" });
+  } else {
+    // Continue to the login page
+    next();
+  }
+}
+
+export default [
+  // dashboard routes START
+  {
+    path: "/confiance/dashboard",
+    name: "dashboard",
+    meta: {
+      title: "Dashboard",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/dashboard-section/index.vue"),
+  },
+  {
+    path: "/confiance/calendar",
+    name: "calendar",
+    meta: {
+      title: "Calendar",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/calendar-section/calendar.vue"),
+  },
+  {
+    path: "/confiance/chat",
+    name: "chat",
+    meta: {
+      title: "Chat",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/chat-section/chat.vue"),
+  },
+  {
+    path: "/confiance/suggestions",
+    name: "suggestions",
+    meta: {
+      title: "Suggestions",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/forms-section/suggest.vue"),
+  },
+  {
+    path: "/confiance/bug-report",
+    name: "bugreport",
+    meta: {
+      title: "Bug Report",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/forms-section/bugreport.vue"),
+  },
+  {
+    path: "/confiance/medical-information",
+    name: "medical-information",
+    meta: {
+      title: "Staff Input",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/medical-information/index.vue"),
+  },
+  {
+    path: "/confiance/staff-input",
+    name: "staff-input",
+    meta: {
+      title: "Medical Information",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/staff-input/index.vue"),
+  },
+  {
+    path: "/confiance/settings",
+    name: "profile-setting",
+    meta: {
+      title: "Settings",
+      authRequired: true,
+      onceLoggedIn,
+    },
+    component: () =>
+      import("@/views/users-specific/confiance/settings-section/setting.vue"),
+  },
+];
