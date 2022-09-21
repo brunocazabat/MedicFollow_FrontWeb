beb482efa6088250cc8f64719a5881b82ad4fbe3
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-21 15:29:04 +0200

    Create dark-mode.vue

---
+++ src/components/frames/nav-bar-components/dark-mode.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "darkmode",
+  methods: {
+    toggleDarkMode() {
+      if (document.documentElement.getAttribute("data-layout-mode") == "dark") {
+        document.documentElement.setAttribute("data-layout-mode", "light");
+      } else {
+        document.documentElement.setAttribute("data-layout-mode", "dark");
+      }
+    },
+  }
+}
+</script>
+
+<template>
+  <button type="button" class="btn btn-soft-secondary btn-sm btn-rounded" @click="toggleDarkMode" data-key="t-darkmode">
+    {{ $t("t-darkmode") }}<em class="bx bx-moon ms-1"></em>
+  </button>
+</template>
