1194d0ee5de3f0be9b4a8c108f9b0125a2f9146e
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-21 16:17:36 +0200

    Display PageHeader only if-admin

--- src/components/frames/page-header.vue
+++ src/components/frames/page-header.vue
@@ -1,34 +1,50 @@
 <script>
+import store from "@/components/state/store";
+
 export default {
-  components: {},
-  props: {
-    title: {
-      type: String,
-      default: "",
+  name: 'PageHeader',
+  data() {
+    return {
+      selected: '',
+      userTypes: [
+        { id: 1, name: 'admin' },
+        { id: 2, name: 'organisation' },
+        { id: 3, name: 'medecin' },
+        { id: 4, name: 'medical' },
+        { id: 5, name: 'patient' },
+        { id: 6, name: 'confiance' },
+        { id: 7, name: 'proche' },
+      ]
+    }
+  },
+  methods: {
+    activateheader() {
+      return store.getters["auth/getuserType"] == "admin";
     },
-    items: {
-      type: Array,
-      default: () => {
-        return [];
-      },
-    },
+    modifyUIType() {
+      store.dispatch("auth/setUI", this.selected.text);
+    }
   },
 };
 </script>
 
 <template>
   <!-- start page title -->
-  <div class="row">
+  <div class="row" v-if="activateheader()">
     <div class="col-12">
-      <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-        <h4 class="mb-sm-0" data-key="t-home">{{ $t("t-home") }}</h4>
-
-        <div class="page-title-right">
-          <ol class="breadcrumb m-0">
-            <li class="breadcrumb-item" v-for="(item, index) in items" :key="index" :class="{ active: item[0] }">
-              <a href="javascript: void(0);">{{ item.text }}</a>
-            </li>
-          </ol>
+      <div class="page-title-box align-items-center justify-content-between">
+        <div class="row">
+          <div class="col-2 align-content-center text-center align-items-center">
+            <h4 class="mb-sm-0">UI SELECTOR ADMIN</h4>
+          </div>
+          <div class="col-1">
+            <select class="form-control" v-model="selected" @change="modifyUIType()">
+              <option v-for="userType in userTypes" v-bind:value="{ id: userType.id, text: userType.name  }"
+                :key="userType.id">{{
+                userType.name }}
+              </option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
