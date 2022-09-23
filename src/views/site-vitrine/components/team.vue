ec54e314823a64066ea689928666a17027746a23
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-23 14:24:06 +0200

    Create team.vue

---
+++ src/views/site-vitrine/components/team.vue
@@ -0,0 +1,205 @@
+<script>
+export default {
+  name: 'team',
+}
+</script>
+  
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <div class="text-center mb-5">
+          <h3 class="mb-3 fw-semibold">
+            NOTRE <span class="text-danger">ÉQUIPE</span>
+          </h3>
+          <p class="text-muted mb-4 ff-secondary">
+            de développeurs qualifiée
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- end row -->
+    <div class="row">
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-1.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Bruno Cazabat</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Président-Directeur Général</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-2.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Simon Bernabeu</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Fondateur</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-3.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Maxime Barbier</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Développeur Front-End</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-4.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Samuel Parayre</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Développeur Mobile</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+    </div>
+    <!-- end row -->
+    <div class="row">
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-5.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Damien Vachier</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Responsable Dev-ops</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-6.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Guillaume Corbet</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Développeur Back-End</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-7.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Malo Moreau</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Développeur Back-End</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+
+      <div class="col-lg-3 col-sm-6">
+        <div class="card">
+          <div class="card-body text-center p-4">
+            <div class="avatar-xl mx-auto mb-4 position-relative">
+              <img src="../images/team/user-8.jpg" alt="" class="img-fluid rounded-circle" />
+              <a href="mailto:contact@medicfollow.fr"
+                class="btn btn-success btn-sm position-absolute bottom-0 end-0 rounded-circle avatar-xs">
+                <div class="avatar-title bg-transparent">
+                  <em class="ri-mail-fill align-bottom"></em>
+                </div>
+              </a>
+            </div>
+            <!-- end card body -->
+            <h5 class="mb-1" style="margin-top: 40px">
+              <a href="pages-profile.html" class="text-body">Alex Limongi</a>
+            </h5>
+            <p class="text-muted mb-0 ff-secondary">Responsable Juridique</p>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+    </div>
+  </div>
+</template>
