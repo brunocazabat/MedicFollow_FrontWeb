737af307d8a9e6c85fac94865c8679bdcac28149
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-23 18:31:06 +0200

    Fix

--- src/components/router/rte-errors.js
+++ src/components/router/rte-errors.js
@@ -32,6 +32,15 @@ export default [
     component: () => import("@/views/UNIVERSAL/errors-section/500.vue"),
   },
   {
+    path: "/404",
+    name: "404-err",
+    meta: {
+      title: "Error 404",
+      onceLoggedIn,
+    },
+    component: () => import("@/views/UNIVERSAL/errors-section/404.vue"),
+  },
+  {
     path: "/:pathMatch(.*)*",
     name: "404-error",
     meta: {
