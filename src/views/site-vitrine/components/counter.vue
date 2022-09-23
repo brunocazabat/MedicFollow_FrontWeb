c796e8568077d599d448fc2134dad227141f053b
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-23 17:22:47 +0200

    Fix count to

--- src/views/site-vitrine/components/counter.vue
+++ src/views/site-vitrine/components/counter.vue
@@ -1,6 +1,11 @@
 <script>
+import { CountTo } from 'vue3-count-to';
+
 export default {
   name: 'counter',
+  components: {
+    CountTo
+  },
 }
 </script>
 
@@ -10,7 +15,7 @@ export default {
       <div class="col-lg-3 col-6">
         <div>
           <h2 class="mb-2">
-            <count-to :startVal="0" :endVal="100" :duration="5000">15</count-to>
+            <count-to :startVal="0" :endVal="15" :duration="4000">15</count-to>
           </h2>
           <div class="text-muted">Beta-Testeurs</div>
         </div>
@@ -20,7 +25,7 @@ export default {
       <div class="col-lg-3 col-6">
         <div>
           <h2 class="mb-2">
-            <count-to :startVal="0" :endVal="24" :duration="5000">30</count-to>+
+            <count-to :startVal="0" :endVal="30" :duration="4000">30</count-to>+
           </h2>
           <div class="text-muted">Retours Positifs</div>
         </div>
@@ -30,7 +35,7 @@ export default {
       <div class="col-lg-3 col-6">
         <div>
           <h2 class="mb-2">
-            <count-to :startVal="0" :endVal="20" :duration="5000">3</count-to>
+            <count-to :startVal="0" :endVal="3" :duration="4000">3</count-to>
           </h2>
           <div class="text-muted">Applications Fonctionnelles</div>
         </div>
@@ -39,7 +44,7 @@ export default {
       <div class="col-lg-3 col-6">
         <div>
           <h2 class="mb-2">
-            <count-to :startVal="0" :endVal="50" :duration="5000">7</count-to>
+            <count-to :startVal="0" :endVal="7" :duration="4000">7</count-to>
           </h2>
           <div class="text-muted">Employés</div>
         </div>
