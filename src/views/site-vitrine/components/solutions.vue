27501eab56586b3e1a19e3181365300eaaa195d8
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-23 14:24:04 +0200

    Create solutions.vue

---
+++ src/views/site-vitrine/components/solutions.vue
@@ -0,0 +1,200 @@
+<script>
+export default {
+  name: 'solutions',
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row align-items-center gy-4">
+      <div class="col-lg-6 order-2 order-lg-1">
+        <div class="text-muted">
+          <h5 class="fs-18 text-uppercase text-success">Le client Mobile</h5>
+          <h4 class="mb-5">Disponible pour les particuliers.</h4>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="vstack gap-2">
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Compatible IOS & Android</h5>
+                    <p class="ff-secondary">
+                      Retrouvez MedicFollow ainsi que ses mises à jour sur les téléphones embarquant des systèmes
+                      IOS
+                      et Android.
+                    </p>
+                  </div>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Données cryptées en local et accès avec l'empreinte digitale*</h5>
+                    <p class="ff-secondary">
+                      Emportez toutes vos données avec vous sans la moindre crainte en cas de perte de votre
+                      téléphone. (* Si votre téléphone est compatible avec le système de reconnaissance
+                      biométrique).
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="vstack gap-2">
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Système de notification intelligent</h5>
+                    <p class="ff-secondary">
+                      Restez au courant des derniers événements ou documents disponibles grâce aux notifications
+                      push.
+                    </p>
+                  </div>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Exportation de votre agenda médical avec les autres services</h5>
+                    <p class="ff-secondary">
+                      Vous utilisez Google Agenda ou Microsoft Outlook ? Liez vos comptes afin d'être sûr de ne rien
+                      manquer.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="mt-4">
+            <a href="https://play.google.com/store/apps/details?id=org.telegram.messenger&gl=FR"
+              class="btn btn-primary">Lien vers le téléchargement de l'application mobile
+              <em class="ri-arrow-right-line align-middle ms-1"></em></a>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-6 col-sm-7 col-10 ms-auto order-1 order-lg-2">
+        <div>
+          <img src="../images/banner/Banner_solution_1.jpg" alt="" class="img-fluid" />
+        </div>
+      </div>
+    </div>
+    <!-- end row -->
+
+    <div class="row align-items-center mt-5 pt-lg-5 gy-4">
+      <div class="col-lg-6 col-sm-7 col-10 mx-auto">
+        <div>
+          <img src="../images/banner/Banner_solution_2.jpg" alt="" class="img-fluid" />
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="text-muted ps-lg-5">
+          <h5 class="fs-18 text-uppercase text-success">Le client Web</h5>
+          <h4 class="mb-5">Disponible dès maintenant, pour les particuliers et les professionnels.</h4>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="vstack gap-2">
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Compatible avec tous les navigateurs internet</h5>
+                    <p class="ff-secondary">
+                      Le client web de MedicFollow est compatible avec tous les navigateurs disponibles.
+                    </p>
+                  </div>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Interface intuitive et professionnelle</h5>
+                    <p class="ff-secondary">
+                      Un gain de temps considérable au quotidien pour les professionnels, une utilisation simple
+                      pour les particuliers.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="vstack gap-2">
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Pas de mise à jour à faire de votre côté</h5>
+                    <p class="ff-secondary">
+                      Un simple redémarrage vous permettra de bénéficier de nos mises à jour, vous serez notifié à
+                      l'arrivée de nouvelles fonctionnalités.
+                    </p>
+                  </div>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="flex-shrink-0 me-2">
+                    <div class="avatar-xs icon-effect">
+                      <div class="avatar-title bg-transparent text-success rounded-circle h2">
+                        <em class="ri-check-fill"></em>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="flex-grow-1">
+                    <h5 class="fs-14 mb-3">Utilisable, peu importe la plate-forme</h5>
+                    <p class="ff-secondary">
+                      Un navigateur et une connexion internet vous suffiront pour profiter des services de
+                      MedicFollow.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="mt-4">
+            <router-link to="/login" class="btn btn-primary">Lien vers le client web
+              <em class="ri-arrow-right-line align-middle ms-1"></em>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+    </div>
+    <!-- end row -->
+  </div>
+</template>
