f116284d18271c1a16e77dad0d5ebcec4c10e84b
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-23 16:43:11 +0200

    Create tarification.vue

---
+++ src/views/site-vitrine/components/tarification.vue
@@ -0,0 +1,175 @@
+<script>
+export default {
+  name: 'tarification',
+}
+</script>
+
+<template>
+  <div class="bg-overlay bg-overlay-pattern"></div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <div class="text-center mb-5">
+          <h1 class="mb-3 ff-secondary fw-semibold lh-base text-uppercase text-success text-center">Pourquoi nous
+            choisir ?</h1>
+          <h4 class="mb-3 fw-semibold">
+            Tarification simplifiée. Pas de frais cachés.<br />Fonctionnalités avancées pour votre organisation
+            médicale.
+          </h4>
+        </div>
+      </div>
+      <!-- end col -->
+    </div>
+    <!-- end row -->
+
+    <div class="row gy-4">
+      <div class="col-lg-6">
+        <div class="card plan-box mb-0">
+          <div class="card-body p-4 m-2">
+            <div class="d-flex align-items-center">
+              <div class="flex-grow-1">
+                <h5 class="mb-1 fw-semibold">Plan Basique</h5>
+                <p class="text-muted mb-0">Pour les organisation médicales</p>
+              </div>
+              <div class="avatar-sm">
+                <div class="avatar-title bg-light rounded-circle text-primary">
+                  <em class="ri-book-mark-line fs-20"></em>
+                </div>
+              </div>
+            </div>
+            <div class="py-4 text-center">
+              <h1>
+                <sup><small>€ </small></sup><span class="ff-secondary fw-bold">1</span>
+                <span class="fs-13 text-muted"> /Patients /Jours</span>
+              </h1>
+            </div>
+
+            <div>
+              <ul class="list-unstyled text-muted vstack gap-3 ff-secondary">
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Jusqu'à <strong>30</strong> Services</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Jusqu'à <strong>300</strong> Médecins</div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Patients <strong>Illimités</strong></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Support <strong>24/7</strong></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Stockage <strong>Illimité</strong></div>
+                  </div>
+                </li>
+
+              </ul>
+              <div class="mt-4">
+                <a href="javascript:void(0);" class="btn btn-soft-success w-100">Commencer</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--end col-->
+      <div class="col-lg-6">
+        <div class="card plan-box mb-0">
+          <div class="card-body p-4 m-2">
+            <div class="d-flex align-items-center">
+              <div class="flex-grow-1">
+                <h5 class="mb-1 fw-semibold">Plan Basique</h5>
+                <p class="text-muted mb-0">Pour les médecins membre</p>
+              </div>
+              <div class="avatar-sm">
+                <div class="avatar-title bg-light rounded-circle text-primary">
+                  <em class="ri-stack-fill fs-20"></em>
+                </div>
+              </div>
+            </div>
+            <div class="py-4 text-center">
+              <h1>
+                <sup><small>€ </small></sup><span class="ff-secondary fw-bold">0.4</span>
+                <span class="fs-13 text-muted"> /(Télé-)Consultations</span>
+              </h1>
+            </div>
+
+            <div>
+              <ul class="list-unstyled text-muted vstack gap-3 ff-secondary">
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">(Télé-)Consultations <strong>Illimités</strong></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Patients <strong>Illimités</strong></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Sans <strong>Frais Supplémentaires</strong></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Support <strong>24/7</strong></div>
+                  </div>
+                </li>
+                <li>
+                  <div class="d-flex">
+                    <div class="flex-shrink-0 text-success me-1">
+                      <em class="ri-checkbox-circle-fill fs-15 align-middle"></em>
+                    </div>
+                    <div class="flex-grow-1">Stockage <strong>Illimité</strong></div>
+                  </div>
+                </li>
+
+              </ul>
+              <div class="mt-4">
+                <a href="javascript:void(0);" class="btn btn-soft-success w-100">Commencer</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--end col-->
+    </div>
+    <!--end row-->
+  </div>
+</template>
