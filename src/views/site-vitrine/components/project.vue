e7469b21fd7faea8245228e22f51697deee7a597
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-23 14:23:58 +0200

    Create project.vue

---
+++ src/views/site-vitrine/components/project.vue
@@ -0,0 +1,196 @@
+<script>
+export default {
+  name: 'project',
+}
+</script>
+
+<template>
+  <div class="container" style="margin-top: 75px">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <div class="text-center mb-5">
+          <h1 class="mb-3 ff-secondary fw-semibold lh-base">
+            NOTRE PROJET
+          </h1>
+          <h3 class="mb-3 text-muted">
+            " Optimiser et rendre plus rapide la communication entre le corps médical en charge du patient et la
+            famille grace à MedicFollow. "
+          </h3>
+          <h5 class="mb-5 text-muted">
+            A l'aide d'une suite d'outils adapté aux particuliers et aux professionnels du corp médical.
+          </h5>
+        </div>
+      </div>
+      <!-- end col -->
+    </div>
+    <!-- end row -->
+
+    <div class="row g-3">
+      <!-- end col -->
+      <div class="col-lg-4">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-arrow-right-s-line align-bottom fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Sécurisé et fiable</h5>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-4">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-arrow-right-s-line align-bottom fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Plus de visibilité</h5>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-4">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-arrow-right-s-line align-bottom fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Meilleure communication</h5>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-4">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-arrow-right-s-line align-bottom fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Gain de temps</h5>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-4">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-arrow-right-s-line align-bottom fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Tranquillité assurée</h5>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-4">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-arrow-right-s-line align-bottom fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Et bien plus</h5>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+
+
+      <div class="col-lg-3">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0 me-3">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-briefcase-5-line fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Rapports Médicaux sécurisés</h5>
+            <p class="text-muted my-3 ff-secondary">
+              Retrouvez tous les documents de votre proche dans un espace unique et sécurisé.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0 me-3">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-slideshow-line fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Prochaines échéances</h5>
+            <p class="text-muted my-3 ff-secondary">
+              Restez informés des prochains rendez-vous de votre proche, où que vous soyez.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0 me-3">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-settings-2-line fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Mise à jours fréquentes</h5>
+            <p class="text-muted my-3 ff-secondary">
+              Tout changement de situation de votre proche vous serra communiquer par une notification.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end col -->
+      <div class="col-lg-3">
+        <div class="d-flex p-3">
+          <div class="flex-shrink-0 me-3">
+            <div class="avatar-sm icon-effect">
+              <div class="avatar-title bg-transparent text-success rounded-circle">
+                <em class="ri-customer-service-line fs-36"></em>
+              </div>
+            </div>
+          </div>
+          <div class="flex-grow-1">
+            <h5 class="fs-18">Vous avez une question ?</h5>
+            <p class="text-muted my-3 ff-secondary">
+              Prenez contact via une messagerie pour poser vos questions vis-à-vis de vos proches.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end row -->
+  </div>
+</template>
