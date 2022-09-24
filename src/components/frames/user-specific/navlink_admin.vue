b4d42d6804327ea85909af45e75622bd4c8dbe6d
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-24 15:45:55 +0200

    Update navlink_admin.vue

--- src/components/frames/user-specific/navlink_admin.vue
+++ src/components/frames/user-specific/navlink_admin.vue
@@ -1,82 +1,30 @@
 <script>
 export default {
   name: 'NavLinkAdmin',
-  data() {
-    return {
-      selected: '',
-      userTypes: [
-        { id: 1, name: 'organisation' },
-        { id: 2, name: 'medecin' },
-        { id: 3, name: 'medical' },
-        { id: 4, name: 'patient' },
-        { id: 5, name: 'confiance' },
-        { id: 6, name: 'proche' },
-      ]
-    }
-  },
 }
 </script>
 
 <template>
   <ul class="navbar-nav h-100" id="navbar-nav">
     <li class="menu-title">
-      <span>MENU SELECTOR ADMIN</span>
-    </li>
-    <li class="nav-item nav-link">
-      <select class="form-control" v-model="selected">
-        <option v-for="userType in userTypes" v-bind:value="{ id: userType.id, text: userType.name  }"
-          :key="userType.id">{{
-          userType.name }}
-        </option>
-      </select>
-    </li>
-    <li class="menu-title">
       <span data-key="t-menu"> {{ $t("t-menu") }}</span>
     </li>
     <li class="nav-item">
-      <router-link class="nav-link menu-link" :to="'/'+selected.text+'/dashboard'">
+      <router-link class="nav-link menu-link" to="/admin/dashboard">
         <em class="ri-home-8-line"></em>
         <span style="padding: 7px" data-key="t-home">{{ $t("t-home") }}</span>
       </router-link>
     </li>
     <li class="nav-item">
-      <router-link class="nav-link menu-link" :to="'/'+selected.text+'/medical-information'">
-        <em class="ri-information-line"></em>
-        <span style="padding: 7px" data-key="t-medicalinformation">{{ $t("t-medicalinformation") }}</span>
+      <router-link class="nav-link menu-link" to="/admin/seereports">
+        <em class="ri-user-settings-line"></em>
+        <span style="padding: 7px" data-key="t-seereports">{{ $t("t-seereports") }}</span>
       </router-link>
     </li>
     <li class="nav-item">
-      <router-link class="nav-link menu-link" :to="'/'+selected.text+'/calendar'">
-        <em class="ri-layout-grid-line"></em>
-        <span style="padding: 7px" data-key="t-calendar">{{ $t("t-calendar") }}</span>
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link class="nav-link menu-link" :to="'/'+selected.text+'/chat'">
-        <em class="ri-pencil-line"></em>
-        <span style="padding: 7px" data-key="t-chat">{{ $t("t-chat") }}</span>
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link class="nav-link menu-link" :to="'/'+selected.text+'/settings'">
+      <router-link class="nav-link menu-link" to="/admin/settings">
         <em class="ri-user-settings-line"></em>
         <span style="padding: 7px" data-key="t-settings">{{ $t("t-settings") }}</span>
-      </router-link>
-    </li>
-    <!-- end Dashboard Menu -->
-    <li class="menu-title">
-      <span data-key="t-contacts"> {{ $t("t-contacts") }}</span>
-    </li>
-    <li class="nav-item">
-      <router-link :to="'/'+selected.text+'/suggestions'" class="nav-link">
-        <em class="ri-star-half-s-line"></em>
-        <span style="padding: 7px" data-key="t-suggest">{{ $t("t-suggest") }}</span>
-      </router-link>
-    </li>
-    <li class="nav-item">
-      <router-link :to="'/'+selected.text+'/bug-report'" class="nav-link">
-        <em class="ri-bug-fill"></em>
-        <span style="padding: 7px" data-key="t-report">{{ $t("t-report") }}</span>
       </router-link>
     </li>
   </ul>
