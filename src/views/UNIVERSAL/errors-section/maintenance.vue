de21df43b793b9a40078d9150959c6e77f38c788
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-24 15:13:23 +0200

    trad maintenance

--- src/views/UNIVERSAL/errors-section/maintenance.vue
+++ src/views/UNIVERSAL/errors-section/maintenance.vue
@@ -36,11 +36,14 @@ export default {
                 </div>
               </div>
               <div class="mb-5 text-white-50">
-                <h1 class="display-5 coming-soon-text">
-                  Site is Under Maintenance
+                <h1 class="display-5 coming-soon-text" data-key="t-maintainance">
+                  {{$t("t-maintainance") }}
                 </h1>
                 <div class="mt-4 pt-2">
-                  <a @click="$router.go(-1)" class="btn btn-success"><em class="mdi mdi-home me-1"></em>Back to home</a>
+                  <router-link to="/" class="btn btn-success" data-key="t-returnhome">
+                    <em class="mdi mdi-home me-1"></em>
+                    {{$t("t-returnhome") }}
+                  </router-link>
                 </div>
               </div>
 
