144d4fdabecff7beb6cd68002f23d5d7ba1b750c
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-24 15:12:58 +0200

    trad 403

--- src/views/UNIVERSAL/errors-section/403.vue
+++ src/views/UNIVERSAL/errors-section/403.vue
@@ -25,9 +25,10 @@ export default {
             <div class="text-center">
               <img src="@/assets/images/errors/offline.gif" alt="error img" class="img-fluid">
               <div class="mt-3">
-                <h3 class="text-uppercase text-white">403 - Forbidden Access 😭</h3>
-                <p class="mb-4 text-white">The page you are looking for can't be accessed.</p>
-                <a @click="$router.go(-1)" class="btn btn-success"><em class="mdi mdi-home me-1"></em>Back to home</a>
+                <h3 class="text-uppercase text-white" data-key="t-forbidden">403 - {{ $t("t-forbidden") }} 😭</h3>
+                <p class="mb-4 text-white" data-key="t-forbiddenpage">{{ $t("t-forbiddenpage") }}</p>
+                <router-link to="/" class="btn btn-success" data-key="t-returnhome"><em class="mdi mdi-home me-1"></em>
+                  {{$t("t-returnhome") }}</router-link>
               </div>
             </div>
           </div><!-- end col -->
