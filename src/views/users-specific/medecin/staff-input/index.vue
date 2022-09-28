9996e5c5e44766926960aae738cb631a829564d6
Simon9991 <simon.bernabeu@epitech.eu>
2022-09-28 12:57:45 +0200

    First page of the form
    Mby needs list of patients as well

--- src/views/users-specific/medecin/staff-input/index.vue
+++ src/views/users-specific/medecin/staff-input/index.vue
@@ -24,6 +24,7 @@ export default {
       <div class="p-2 col-xl-12">
         <div class="card">
           <div class="card-body">
+
             <!-- Title + muted text -->
             <h2 class="text-primary">Select a patient</h2>
             <p class="text-muted">Here you can select a patient you visited to then input the latest medical
@@ -31,26 +32,43 @@ export default {
               date of birth are.</p>
 
             <!-- Input fields -->
-            <div class="row">
-
+            <div class="p-3 row">
               <!-- First name -->
-              <div class="col-sm-3">
-                <div class="mb-3">
-                  <label for="patientFirstName" class="form-label" data-key="t-firstname">{{
-                  $t("t-firstname")
-                  }}</label>
-                  <input type="text" class="form-control" id="patientFirstName" placeholder="Enter first name"
-                    value="" />
-                </div>
+              <div class="col-sm-5">
+                <label for="patientFirstName" class="form-label" data-key="t-firstname">{{
+                $t("t-firstname")
+                }}</label>
+                <input type="text" class="form-control" id="patientFirstName" placeholder="Enter first name" value="" />
               </div>
 
               <!-- Last name -->
-              <div class="col-sm-3">
-                <div class="mb-3">
-                  <label for="patientLastName" class="form-label" data-key="t-lastname">{{ $t("t-lastname")
-                  }}</label>
-                  <input type="text" class="form-control" id="patientLastName" placeholder="Enter last name" value="" />
-                </div>
+              <div class="col-sm-5">
+                <label for="patientLastName" class="form-label" data-key="t-lastname">{{ $t("t-lastname")
+                }}</label>
+                <input type="text" class="form-control" id="patientLastName" placeholder="Enter last name" value="" />
+              </div>
+            </div>
+
+            <!-- Social security number + Date of Birth -->
+            <div class="p-3 row">
+
+              <!-- Social Security Number -->
+              <div class="col-sm-5">
+                <label for="patientSocialSecurityNumber" class="form-label" data-key="">Social Security Number</label>
+                <input type="text" class="form-control" id="patientSocialSecurityNumber" placeholder="Enter last name"
+                  value="" required />
+              </div>
+
+              <!-- Date of Birth -->
+              <div class="col-sm-5 mb-5">
+                <label for="patientDoB" class="form-label" data-key="">Date of Birth</label>
+                <input type="text" class="form-control" id="patientDoB" placeholder="Enter last name" value=""
+                  required />
+              </div>
+
+              <!-- Next Button -->
+              <div class="p-3">
+                <a class="btn btn-primary col-sm-1" href="#">NEXT</a>
               </div>
             </div>
           </div>
