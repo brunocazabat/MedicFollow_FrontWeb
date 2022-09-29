2e9cb45494fa9407cd97d35b78d00d27d8daf695
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-29 10:24:57 +0200

    Ajout d'une case 0 pour missing internet

--- src/views/UNIVERSAL/log-account-section/loading-screen.vue
+++ src/views/UNIVERSAL/log-account-section/loading-screen.vue
@@ -38,6 +38,10 @@ export default {
             this.setLogOut();
             this.$router.back();
             break;
+          case 0:
+            this.isAuthError = true;
+            this.authError = "No internet connection";
+            break;
           default:
             this.isAuthError = true;
             this.authError = "An unknown error occurred";
