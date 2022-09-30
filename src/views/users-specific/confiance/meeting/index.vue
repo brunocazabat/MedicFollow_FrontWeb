8326da9c4879b1b8beb34e4bba7d1608cde0ba1c
Simon9991 <simon.bernabeu@epitech.eu>
2022-09-30 10:19:47 +0200

    Added prev btn + title

--- src/views/users-specific/confiance/meeting/index.vue
+++ src/views/users-specific/confiance/meeting/index.vue
@@ -25,7 +25,20 @@ export default {
 
       <div class="p-2">
         <h2 class="text-primary" data-key="t-chooseameeting">{{$t("t-chooseameeting")}}</h2>
-        <p class="text-muted forceserif" data-key="t-plsfillmeetinginfo">{{$t("t-plsfillmeetinginfo")}}</p>
+        <p class="text-muted" data-key="t-plsfillmeetinginfo">{{$t("t-plsfillmeetinginfo")}}</p>
+      </div>
+
+      <div class="p-2 card">
+        <div class="body-card">
+          <div class="p-3 mb-3">
+            <button class="lh-1 btn" data-key="t-previousstep"><em class="ri-arrow-left-line"></em>
+              {{$t("t-previousstep")}}</button>
+          </div>
+          <div class="p-3">
+            <p class="form-label font-size-large" data-key="t-choosemeetingdate">
+              <strong>{{$t("t-choosemeetingdate")}}</strong></p>
+          </div>
+        </div>
       </div>
 
 
