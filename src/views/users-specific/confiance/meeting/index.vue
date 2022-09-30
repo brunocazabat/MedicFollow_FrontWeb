e9dded5874aa23c0a762e423c4569be68602af77
Simon9991 <simon.bernabeu@epitech.eu>
2022-09-30 15:45:33 +0200

    UI changes for select doctor

--- src/views/users-specific/confiance/meeting/index.vue
+++ src/views/users-specific/confiance/meeting/index.vue
@@ -6,6 +6,8 @@ export default {
   data() {
     return {
       viewID: 0,
+      pickedDoctor: '',
+      test: 0,
     }
 
   },
@@ -31,7 +33,6 @@ export default {
 
     <div class="project-wrapper meeting-form">
 
-
       <div class="p-2">
         <h2 class="text-primary" data-key="t-chooseameeting">{{$t("t-chooseameeting")}}</h2>
         <p class="text-muted" data-key="t-plsfillmeetinginfo">{{$t("t-plsfillmeetinginfo")}}</p>
@@ -42,7 +43,44 @@ export default {
 
           <!-- CHOOSE DOCTOR DIV -->
           <div v-if="viewID === 0">
-            <div class="p-3 col-lg-3">
+
+            <div class="basic-card-border col-sm-12 dl-card">
+              <a class="nav-link menu-link col-sm-12 font-size-medium two-percent-height center-items"
+                href="#meetingChooseDoctor" data-bs-toggle="collapse" role="button" aria-expanded="false"
+                aria-controls="meetingDateCollapse">
+                <span>{{$t("t-selectdoctormeeting")}}... <em class="ri-arrow-down-line lh-1 center-items"></em></span>
+              </a>
+
+              <div class="collapse col-sm-12 basic-padding" id="meetingChooseDoctor">
+                <hr class="mf-divider">
+
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" id="doc1GridCheck" value="Doc1" v-model="pickedDoctor" />
+                  <label class="form-check-label font-size-medium" for="doc1GridCheck">Dr. BARBIER Maxime</label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" id="doc2GridCheck" value="Doc2" v-model="pickedDoctor" />
+                  <label class="form-check-label font-size-medium" for="doc2GridCheck">Dr. BERNABEU Simon</label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" id="doc3GridCheck" value="Doc3" v-model="pickedDoctor" />
+                  <label class="form-check-label font-size-medium" for="doc3GridCheck">Dr. CAZABAT Bruno</label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" id="doc4GridCheck" value="Doc4" v-model="pickedDoctor" />
+                  <label class="form-check-label font-size-medium" for="doc4GridCheck">Dr. CORBET Guillaume</label>
+                </div>
+
+              </div>
+
+            </div>
+
+
+
+            <!-- <div class="p-3 col-lg-6">
               <select class="form-select" id="chooseDoctor" required>
                 <option selected data-key="t-selectdoctormeeting">{{$t("t-selectdoctormeeting")}}...</option>
                 <option value="doc1">Dr. CAZABAT Bruno</option>
@@ -50,7 +88,8 @@ export default {
                 <option value="doc3">Dr. BARBIER Bruno</option>
                 <option value="doc4">Dr. CORBET Guillaume</option>
               </select>
-            </div>
+            </div> -->
+
           </div>
 
           <!-- CONSULTING DOCTOR QUESTION -->
@@ -107,7 +146,8 @@ export default {
           :disabled="viewID === 0"><em class="ri-arrow-left-line center-items"></em>
           {{$t("t-previousstep")}}</button>
         <button class="lh-1 btn btn-primary font-size-medium col-lg-3" v-on:click="nextView()"
-          :disabled="viewID === 2">{{$t("t-nextstep")}} <em class="ri-arrow-right-line center-items"></em></button>
+          :disabled="viewID === 2 || pickedDoctor === ''">{{$t("t-nextstep")}} <em
+            class="ri-arrow-right-line center-items"></em></button>
       </div>
 
 
