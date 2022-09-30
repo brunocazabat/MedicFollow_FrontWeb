58f5bdcc9860b3bde38099e6fe3cb023414896c6
Simon9991 <simon.bernabeu@epitech.eu>
2022-09-30 11:54:32 +0200

    Online Meeting 3 form pages
    :feelsgood:

--- src/views/users-specific/confiance/meeting/index.vue
+++ src/views/users-specific/confiance/meeting/index.vue
@@ -4,12 +4,23 @@ import footermodule from "@/components/login-components/footer-module.vue";
 
 export default {
   data() {
+    return {
+      viewID: 0,
+    }
 
   },
   components: {
     Layout,
-    footermodule,
+    footermodule
   },
+  methods: {
+    nextView() {
+      this.viewID += 1;
+    },
+    prevView() {
+      this.viewID -= 1;
+    }
+  }
 
 }
 
@@ -28,17 +39,87 @@ export default {
         <p class="text-muted" data-key="t-plsfillmeetinginfo">{{$t("t-plsfillmeetinginfo")}}</p>
       </div>
 
-      <div class="p-2 card">
+      <div class="p-3 card">
         <div class="body-card">
-          <div class="p-3 mb-3">
-            <button class="lh-1 btn" data-key="t-previousstep"><em class="ri-arrow-left-line"></em>
-              {{$t("t-previousstep")}}</button>
+
+
+          <div v-if="viewID === 0">
+            <div class="p-3 mb-3 row">
+              <button class="lh-1 btn font-size-medium col-lg-3" v-on:click="nextView()"
+                data-key="t-nextstep">{{$t("t-nextstep")}}<em class="ri-arrow-right-line"></em></button>
+            </div>
+
+            <div class="p-3 col-lg-3">
+              <select class="form-select" id="chooseDoctor" required>
+                <option selected data-key="t-selectdoctormeeting">{{$t("t-selectdoctormeeting")}}...</option>
+                <option value="doc1">Dr. CAZABAT Bruno</option>
+                <option value="doc2">Dr. BERNABEU Bruno</option>
+                <option value="doc3">Dr. BARBIER Bruno</option>
+                <option value="doc4">Dr. CORBET Guillaume</option>
+              </select>
+            </div>
           </div>
-          <div class="p-3">
-            <p class="form-label font-size-large" data-key="t-choosemeetingdate">
-              <strong>{{$t("t-choosemeetingdate")}}</strong></p>
+
+          <div v-if="viewID === 1">
+            <div class="p-3 mb-3 row">
+              <button class="lh-1 btn font-size-medium col-lg-3" v-on:click="prevView()" data-key="t-previousstep"><em
+                  class="ri-arrow-left-line"></em>
+                {{$t("t-previousstep")}}</button>
+              <button class="lh-1 btn font-size-medium col-lg-3" v-on:click="nextView()"
+                data-key="t-nextstep">{{$t("t-nextstep")}} (debug) <em class="ri-arrow-right-line"></em></button>
+            </div>
+
+            <div class="p-3">
+              <p class="form-label font-size-large" data-key="t-haveyoumetdoctor">
+                <strong>{{$t("t-haveyoumetdoctor")}}</strong>
+              </p>
+            </div>
+
+            <div class="p-3">
+              <p class="font-size-medium">Yes / No (take from previous medical input form)</p>
+            </div>
+          </div>
+
+
+
+
+          <div v-if="viewID === 2">
+            <div class="p-3 mb-3">
+              <button class="lh-1 btn font-size-medium" v-on:click="prevView()" data-key="t-previousstep"><em
+                  class="ri-arrow-left-line"></em>
+                {{$t("t-previousstep")}}</button>
+            </div>
+            <div class="p-3">
+              <p class="form-label font-size-large" data-key="t-choosemeetingdate">
+                <strong>{{$t("t-choosemeetingdate")}}</strong>
+              </p>
+            </div>
+            <div class="p-3">
+              <a class="btn nav-link menu-link col-sm-4 font-size-medium mb-2 two-percent-height"
+                href="#meetingDateCollapse" data-bs-toggle="collapse" role="button" aria-expanded="false"
+                aria-controls="meetingDateCollapse">
+                <span>Friday, September 30 <em class="ri-arrow-down-line lh-1"></em></span>
+              </a>
+              <div class="collapse col-sm-4" id="meetingDateCollapse">
+
+                <button class="btn btn-primary mb-2 col-sm-2">9:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">15:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">15:30</button>
+                <button class="btn btn-primary mb-2 col-sm-2">17:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">18:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">19:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">16:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">16:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">16:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">16:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">16:00</button>
+                <button class="btn btn-primary mb-2 col-sm-2">16:00</button>
+
+              </div>
+            </div>
           </div>
         </div>
+
       </div>
 
 
