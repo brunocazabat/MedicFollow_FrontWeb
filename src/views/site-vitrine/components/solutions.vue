d0bf405329d0a11763ba5ad2ef4653d998ea8be2
bruno cazabat <bruno.cazabat@epitech.eu>
2022-10-03 01:21:07 +0200

    Update solutions.vue

--- src/views/site-vitrine/components/solutions.vue
+++ src/views/site-vitrine/components/solutions.vue
@@ -7,11 +7,12 @@ export default {
 <template>
   <div class="container">
     <div class="row align-items-center gy-4">
-      <h1 class="mb-3 ff-secondary fw-semibold lh-base text-uppercase text-success text-center">NOS SOLUTIONS</h1>
+      <h1 class="mb-3 ff-secondary fw-semibold lh-base text-uppercase text-success text-center"
+        data-key="t-oursolution">{{ $t("t-oursolution") }}</h1>
       <div class="col-lg-6 order-2 order-lg-1">
         <div class="text-muted">
-          <h3 class="fs-28 text-uppercase text-success mb-4">Le client Mobile</h3>
-          <h4 class="mb-5">Disponible pour les particuliers.</h4>
+          <h3 class="fs-28 text-uppercase text-success mb-4" data-key="t-mobileclient">{{ $t("t-mobileclient") }}</h3>
+          <h4 class="mb-5" data-key="t-partavailable">{{ $t("t-partavailable") }}</h4>
           <div class="row">
             <div class="col-sm-6">
               <div class="vstack gap-2">
@@ -24,11 +25,8 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Compatible IOS & Android</h5>
-                    <p class="ff-secondary">
-                      Retrouvez MedicFollow ainsi que ses mises à jour sur les téléphones embarquant des systèmes
-                      IOS
-                      et Android.
+                    <h5 class="fs-14 mb-3" data-key="t-iosandroid">{{ $t("t-iosandroid") }}</h5>
+                    <p class="ff-secondary" data-key="t-findiosandroid">{{ $t("t-findiosandroid") }}
                     </p>
                   </div>
                 </div>
@@ -41,11 +39,8 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Données cryptées en local et accès avec l'empreinte digitale*</h5>
-                    <p class="ff-secondary">
-                      Emportez toutes vos données avec vous sans la moindre crainte en cas de perte de votre
-                      téléphone. (* Si votre téléphone est compatible avec le système de reconnaissance
-                      biométrique).
+                    <h5 class="fs-14 mb-3" data-key="t-crypted">{{ $t("t-crypted") }}</h5>
+                    <p class="ff-secondary" data-key="t-safedata">{{ $t("t-safedata") }}
                     </p>
                   </div>
                 </div>
@@ -62,10 +57,8 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Système de notification intelligent</h5>
-                    <p class="ff-secondary">
-                      Restez au courant des derniers événements ou documents disponibles grâce aux notifications
-                      push.
+                    <h5 class="fs-14 mb-3" data-key="t-intellinotif">{{ $t("t-intellinotif") }}</h5>
+                    <p class="ff-secondary" data-key="t-stayupnotif">{{ $t("t-stayupnotif") }}
                     </p>
                   </div>
                 </div>
@@ -78,10 +71,8 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Exportation de votre agenda médical avec les autres services</h5>
-                    <p class="ff-secondary">
-                      Vous utilisez Google Agenda ou Microsoft Outlook ? Liez vos comptes afin d'être sûr de ne rien
-                      manquer.
+                    <h5 class="fs-14 mb-3" data-key="t-exportagenda">{{ $t("t-exportagenda") }}</h5>
+                    <p class="ff-secondary" data-key="t-googleoutlook">{{ $t("t-googleoutlook") }}
                     </p>
                   </div>
                 </div>
@@ -89,8 +80,8 @@ export default {
             </div>
           </div>
           <div class="mt-4">
-            <a href="https://play.google.com/store/apps/details?id=org.telegram.messenger&gl=FR"
-              class="btn btn-primary">Lien vers le téléchargement de l'application mobile
+            <a href="https://play.google.com/store/apps/details?id=org.telegram.messenger&gl=FR" class="btn btn-primary"
+              data-key="t-downloadmoblink">{{ $t("t-downloadmoblink") }}
               <em class="ri-arrow-right-line align-middle ms-1"></em></a>
           </div>
         </div>
@@ -112,8 +103,8 @@ export default {
       </div>
       <div class="col-lg-6">
         <div class="text-muted ps-lg-5">
-          <h3 class="fs-28 text-uppercase text-success mb-4">Le client Web</h3>
-          <h4 class="mb-5">Disponible dès maintenant, pour les particuliers et les professionnels.</h4>
+          <h3 class="fs-28 text-uppercase text-success mb-4" data-key="t-webclient">{{ $t("t-webclient") }}</h3>
+          <h4 class="mb-5" data-key="t-availpropart">{{ $t("t-availpropart") }}</h4>
           <div class="row">
             <div class="col-sm-6">
               <div class="vstack gap-2">
@@ -126,9 +117,9 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Compatible avec tous les navigateurs internet</h5>
-                    <p class="ff-secondary">
-                      Le client web de MedicFollow est compatible avec tous les navigateurs disponibles.
+                    <h5 class="fs-14 mb-3" data-key="t-allcompat">{{ $t("t-allcompat") }}
+                    </h5>
+                    <p class="ff-secondary" data-key="t-allcompatbrows">{{ $t("t-allcompatbrows") }}
                     </p>
                   </div>
                 </div>
@@ -141,10 +132,8 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Interface intuitive et professionnelle</h5>
-                    <p class="ff-secondary">
-                      Un gain de temps considérable au quotidien pour les professionnels, une utilisation simple
-                      pour les particuliers.
+                    <h5 class="fs-14 mb-3" data-key="t-interfaceinfo">{{ $t("t-interfaceinfo") }}</h5>
+                    <p class="ff-secondary" data-key="t-interfaceinfoutil">{{ $t("t-interfaceinfoutil") }}
                     </p>
                   </div>
                 </div>
@@ -161,10 +150,8 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Pas de mise à jour à faire de votre côté</h5>
-                    <p class="ff-secondary">
-                      Un simple redémarrage vous permettra de bénéficier de nos mises à jour, vous serez notifié à
-                      l'arrivée de nouvelles fonctionnalités.
+                    <h5 class="fs-14 mb-3" data-key="t-noupdate">{{ $t("t-noupdate") }}</h5>
+                    <p class="ff-secondary" data-key="t-justreload">{{ $t("t-justreload") }}
                     </p>
                   </div>
                 </div>
@@ -177,10 +164,9 @@ export default {
                     </div>
                   </div>
                   <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-3">Utilisable, peu importe la plate-forme</h5>
-                    <p class="ff-secondary">
-                      Un navigateur et une connexion internet vous suffiront pour profiter des services de
-                      MedicFollow.
+                    <h5 class="fs-14 mb-3" data-key="t-allplatform">{{ $t("t-allplatform") }}</h5>
+                    <p class="ff-secondary" data-key="t-justinternet">{{ $t("t-justinternet") }}
+
                     </p>
                   </div>
                 </div>
@@ -188,7 +174,7 @@ export default {
             </div>
           </div>
           <div class="mt-4">
-            <router-link to="/login" class="btn btn-primary">Lien vers le client web
+            <router-link to="/login" class="btn btn-primary" data-key="t-linkwebclient">{{ $t("t-linkwebclient") }}
               <em class="ri-arrow-right-line align-middle ms-1"></em>
             </router-link>
           </div>
