2e75005e95ed2d7b356cdf1695125220f77e9760
bruno cazabat <bruno.cazabat@epitech.eu>
2022-10-03 01:21:04 +0200

    Update signupnow.vue

--- src/views/site-vitrine/components/signupnow.vue
+++ src/views/site-vitrine/components/signupnow.vue
@@ -10,15 +10,15 @@ export default {
     <div class="row align-items-center gy-4">
       <div class="col-sm">
         <div>
-          <h4 class="text-white mb-0 fw-semibold">
-            Vous êtes un professionel de la santé, vous êtes une organisation à but médical ?
+          <h4 class="text-white mb-0 fw-semibold" data-key="t-signupnow">{{ $t("t-signupnow") }}
           </h4>
         </div>
       </div>
       <!-- end col -->
       <div class="col-sm-auto">
         <div>
-          <router-link to="/register-mail" class="btn btn-light" style="margin:5px">Sign Up
+          <router-link to="/register-mail" class="btn btn-light" style="margin:5px" data-key="t-signup">{{
+          $t("t-signup") }}
           </router-link>
         </div>
       </div>
