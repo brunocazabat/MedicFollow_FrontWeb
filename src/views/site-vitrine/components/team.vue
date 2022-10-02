fdf47c8af118c08835eb10ff91d756f2f5adda53
bruno cazabat <bruno.cazabat@epitech.eu>
2022-10-03 01:21:12 +0200

    Update team.vue

--- src/views/site-vitrine/components/team.vue
+++ src/views/site-vitrine/components/team.vue
@@ -9,9 +9,9 @@ export default {
     <div class="row justify-content-center">
       <div class="col-lg-8">
         <div class="text-center mb-5">
-          <h1 class="mb-3 ff-secondary fw-semibold lh-base text-uppercase text-success text-center">notre équipe</h1>
-          <h3 class="mb-3 fw-semibold">
-            de développeurs qualifiés
+          <h1 class="mb-3 ff-secondary fw-semibold lh-base text-uppercase text-success text-center"
+            data-key="t-ourteam">{{ $t("t-ourteam") }}</h1>
+          <h3 class="mb-3 fw-semibold" data-key="t-qualifieddev">{{ $t("t-qualifieddev") }}
           </h3>
         </div>
       </div>
@@ -34,7 +34,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Bruno Cazabat</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Président-Directeur Général</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-pdg">{{ $t("t-pdg") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -56,7 +56,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Simon Bernabeu</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Fondateur</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-ceo">{{ $t("t-ceo") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -78,7 +78,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Maxime Barbier</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Développeur Front-End</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-devmobile">{{ $t("t-devmobile") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -100,7 +100,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Samuel Parayre</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Développeur Mobile</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-devmobile">{{ $t("t-devmobile") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -125,7 +125,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Damien Vachier</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Responsable Dev-ops</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-resdevop">{{ $t("t-resdevop") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -147,7 +147,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Guillaume Corbet</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Développeur Back-End</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-devback">{{ $t("t-devback") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -169,7 +169,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Malo Moreau</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Développeur Back-End</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-devback">{{ $t("t-devback") }}</p>
           </div>
         </div>
         <!-- end card -->
@@ -192,7 +192,7 @@ export default {
             <h5 class="mb-1" style="margin-top: 40px">
               <a href="pages-profile.html" class="text-body">Alex Limongi</a>
             </h5>
-            <p class="text-muted mb-0 ff-secondary">Responsable Juridique</p>
+            <p class="text-muted mb-0 ff-secondary" data-key="t-resjuri">{{ $t("t-resjuri") }}</p>
           </div>
         </div>
         <!-- end card -->
