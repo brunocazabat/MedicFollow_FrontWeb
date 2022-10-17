a0a1e7cd3ece7a2ba6172a298067437e571e5942
bruno cazabat <bruno.cazabat@epitech.eu>
2022-10-17 13:26:48 +0200

    Ho Simon les declaration de pageheader

--- src/components/staff-input-components/calendar.vue
+++ src/components/staff-input-components/calendar.vue
@@ -32,17 +32,6 @@ export default {
   },
   data() {
     return {
-      title: 'Calendar',
-      items: [
-        {
-          text: 'Apps',
-          href: '/'
-        },
-        {
-          text: 'Calendar',
-          active: true
-        }
-      ],
       calendarOptions: {
         timeZone: 'local',
         droppable: true,
