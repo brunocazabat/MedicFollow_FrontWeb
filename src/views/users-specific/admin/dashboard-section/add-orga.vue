7b792fd48cc749163827e43ed342046d2b6fd599
bruno cazabat <bruno.cazabat@epitech.eu>
2022-10-18 14:31:04 +0200

    Small adjustement of UI and visuals

--- src/views/users-specific/admin/dashboard-section/add-orga.vue
+++ src/views/users-specific/admin/dashboard-section/add-orga.vue
@@ -8,9 +8,11 @@ export default {
   <div class="card dashboardcard">
     <div class="card-body align-content-center align-items-center text-center">
       <div class="row">
-        <h1><em class="ri-folder-user-line text-muted mb-3"></em><em class="ri-add-line text-muted mb-3"></em>
+        <h1 class="d-none d-xl-block ms-1 mb-3 ff-secondary user-name-text"><em class="ri-folder-user-line"></em><em
+            class="ri-add-line"></em>
         </h1>
-        <h2 class="mb-3 text-center text-muted">AJOUTER ORGANISATION</h2>
+        <h2 class="d-none d-xl-block ms-1 mb-3 ff-secondary fw-semibold user-name-text text-muted">Ajouter une
+          Organisation</h2>
         <div class="card-footer border-0">
           <div class="row">
             <div class="text-center">
