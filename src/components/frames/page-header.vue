33573a566ebfc8783a7f006b701a7ee40c7a8efd
bruno cazabat <bruno.cazabat@epitech.eu>
2022-10-19 21:54:45 +0200

    Add new tools for admins

--- src/components/frames/page-header.vue
+++ src/components/frames/page-header.vue
@@ -35,7 +35,7 @@ export default {
       <div class="page-title-box align-items-center">
         <div class="row">
           <div class="col-2" style="display: flex; justify-content: center; text-align: center; align-items: center;">
-            <h4 class="mb-sm-0">UI SELECTOR ADMIN</h4>
+            <h4 class="mb-sm-0">MENU SELECTOR ADMIN</h4>
           </div>
           <div class="col-1">
             <select class="form-control" v-model="selected" @change="modifyUIType()">
@@ -44,6 +44,44 @@ export default {
                 userType.name }}
               </option>
             </select>
+          </div>
+          <div class="col-2" style="display: flex; justify-content: center; text-align: center; align-items: center;">
+            <h4 class="mb-sm-0">DASHBOARD SELECTOR ADMIN</h4>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/admin/dashboard" class="btn btn-primary text-uppercase">
+              Admin Dashboard
+            </RouterLink>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/organisation/dashboard" class="btn btn-primary text-uppercase">
+              Organisation Dashboard
+            </RouterLink>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/docteur/dashboard" class="btn btn-primary text-uppercase">
+              Docteur Dashboard
+            </RouterLink>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/personnel/dashboard" class="btn btn-primary text-uppercase">
+              Personnel Dashboard
+            </RouterLink>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/patient/dashboard" class="btn btn-primary text-uppercase">
+              Patient Dashboard
+            </RouterLink>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/confiance/dashboard" class="btn btn-primary text-uppercase">
+              Confiance Dashboard
+            </RouterLink>
+          </div>
+          <div class="col-1">
+            <RouterLink to="/proche/dashboard" class="btn btn-primary text-uppercase">
+              Proche Dashboard
+            </RouterLink>
           </div>
         </div>
       </div>
