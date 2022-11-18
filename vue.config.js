89563cf278fa1a826bfba83bd35a744ff2bfec54
bruno cazabat <bruno.cazabat@epitech.eu>
2022-11-18 18:55:18 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 module.exports = {
   configureWebpack: {
     plugins: [new Dotenv()],
   },
+  outputDir: path.resolve("/usr/share/nginx/www"),
 };
