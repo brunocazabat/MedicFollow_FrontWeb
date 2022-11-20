41a5de84f90bdd42cda84f0119d740f91a9c58a6
bruno cazabat <bruno.cazabat@epitech.eu>
2022-11-20 21:27:08 +0100

    Moving Cookie component into separated instance

--- src/components/view-related/layout/layout-components/index.js
+++ src/components/view-related/layout/layout-components/index.js
@@ -2,5 +2,6 @@ import NavBar from "./nav-bar.vue";
 import MenuMedicFollow from "./menu.vue";
 import RightBar from "./right-bar.vue";
 import PageHeader from "./page-header.vue";
+import Cookies from "./cookies.vue";
 
-export { NavBar, MenuMedicFollow, RightBar, PageHeader };
+export { NavBar, MenuMedicFollow, RightBar, PageHeader, Cookies };
