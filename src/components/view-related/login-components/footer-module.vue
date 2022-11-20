7a9dc25eaf51f3643b129f2d7205a045ff71cc27
bruno cazabat <bruno.cazabat@epitech.eu>
2022-11-20 02:45:17 +0100

    Fix prettyer

--- src/components/view-related/login-components/footer-module.vue
+++ src/components/view-related/login-components/footer-module.vue
@@ -12,9 +12,8 @@ export default {
           <div class="text-center">
             <p class="mb-0 text-muted" data-key="t-footer t-footer-bis">
               &copy; {{ new Date().getFullYear() }} {{ $t("t-footer") }}
-              <em class="mdi mdi-heart text-danger"></em> {{
-              $t("t-footer-bis")
-              }}
+              <em class="mdi mdi-heart text-danger"></em>
+              {{ $t("t-footer-bis") }}
             </p>
           </div>
         </div>
